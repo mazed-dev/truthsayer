475a2a8776261c18bb6856de52e2a5f09390ba81
akindyakov <akindyakov@gmail.com>
2020-06-02 10:05:49 +0100

    ad"

--- src/UploadFile.js
+++ src/UploadFile.js
@@ -24,6 +24,9 @@ import {
 import axios from "axios";
 import queryString from "query-string";
 import { withRouter } from "react-router-dom";
+import remoteErrorHandler from "./remoteErrorHandler";
+
+const hash = require("object-hash");
 
 const FormData = require("form-data");
 
@@ -31,44 +34,69 @@ class UploadFile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      uploaded_nids: [],
+      uploads: [], // [{filename: "", nid: "", local_id: "some local id"}]
     };
     this.fileInputRef = React.createRef();
   }
 
   handleChange = () => {
-    console.log("Change", this.fileInputRef.current.files);
     this.submitFiles(this.fileInputRef.current.files);
   };
 
   submitFiles(files) {
+    let new_uploads = [];
     let data = new FormData();
     for (var i = 0; i < files.length; i++) {
       let file = files.item(i);
-      data.append(i, file, file.name);
+      let local_id = hash([file.name, Math.random()]);
+      data.append(local_id, file, file.name);
+      new_uploads.push({
+        filename: file.name,
+        local_id: local_id,
+        nid: null,
+      });
+      this.setState({
+        uploads: this.state.uploads.concat(new_uploads),
+      });
     }
     const config = {
       headers: { "content-type": "multipart/form-data" },
     };
-    axios.post("/api/batch/node", data, config).then((res) => {
-      console.log("Test response", res);
-    });
+    axios
+      .post("/api/batch/node", data, config)
+      .then((res) => {
+        if (res) {
+          for (let local_id in res.data.nameToNid) {
+            let uploads = this.state.uploads;
+            let ind = uploads.findIndex((item) => {
+              return item.local_id === local_id;
+            });
+            if (ind < 0) {
+              continue;
+            }
+            uploads[ind].nid = res.data.nameToNid[local_id];
+            this.setState({
+              uploads: uploads,
+            });
+          }
+        }
+      })
+      .catch(remoteErrorHandler(this.props.history));
   }
 
   render() {
     // TODO(akindyakov): Continue here
-    const rows = this.state.uploaded_nids.map((nid) => {
-      return (<Row className="m-4">
-        <Col>
-          <p>File</p>
-        </Col>
-        <Col>
-          <p>frst</p>
-        </Col>
-        <Col>
-          <p>second</p>
-        </Col>
-      </Row>);
+    const rows = this.state.uploads.map((item) => {
+      return (
+        <Row className="m-4">
+          <Col>
+            <p>{item.filename}</p>
+          </Col>
+          <Col>
+            <p>{item.nid}</p>
+          </Col>
+        </Row>
+      );
     });
     return (
       <Container>
