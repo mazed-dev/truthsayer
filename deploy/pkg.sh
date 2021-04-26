764e34e4939ab722fa9e832ec301ade70b14643a
akindyakov <akindyakov@gmail.com>
2021-04-26 22:45:24 +0100

    Remove debug logging from deploy script

--- deploy/pkg.sh
+++ deploy/pkg.sh
@@ -1,6 +1,6 @@
 #/usr/bin/env bash
 
-set -x -e
+set -e
 
 # tar --create --bzip2 [--file ARCHIVE] [OPTIONS] [FILE...]
 # tar {--diff|--compare} [--file ARCHIVE] [OPTIONS] [FILE...]
@@ -8,6 +8,7 @@ set -x -e
 # tar --append [-f ARCHIVE] [OPTIONS] [FILE...]
 
 ## Env
+echo "Set up environment"
 
 _NAME="truthsayer"
 _VERSION="$(date +'%Y.%m.%d-%H.%M.%S')"
@@ -24,11 +25,13 @@ _PKG_FILE="${_NAME}-${_VERSION}.tag.gz"
 _PKG_PATH="${_TARGET_DIR}/${_PKG_FILE}"
 
 ## Build
+echo "Build"
 
 yarn build
 
 ## Pack
 
+echo "Pack"
 tar \
   --create \
   --gzip \
@@ -37,6 +40,7 @@ tar \
 
 ## Ready
 
-echo "Package is ready!"
-echo "Copy it to the server by : scp -P 5326 ${_PKG_PATH} \[2a03:b0c0:1:e0::443:5001\]:"
-echo "Unpack on the service by : tar --extract --gzip --file=./${_PKG_FILE}"
+echo "To finish the deploy run:"
+echo "1. % scp -P 5326 ${_PKG_PATH} \[2a03:b0c0:1:e0::443:5001\]:"
+echo "2. % ssh 2a03:b0c0:1:e0::443:5001 -p 5326 -A"
+echo "3. % tar --extract --gzip --file=./${_PKG_FILE}"
