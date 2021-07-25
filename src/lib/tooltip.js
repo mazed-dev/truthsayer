6d10a5671b4d82df10be37b0e998dc89545af97b
akindyakov <akindyakov@gmail.com>
2021-07-25 16:43:41 +0100

    Draw tooltip with z-index 1000 to make sure it is rendered over everything

--- src/lib/tooltip.js
+++ src/lib/tooltip.js
@@ -1,21 +1,15 @@
 import React from 'react'
 
 import './tooltip.css'
-import styles from './tooltip.module.css'
 
 import { jcss } from './../util/jcss'
 
 // https://www.w3schools.com/css/css_tooltip.asp
-
-export class HoverTooltip extends React.Component {
-  render() {
-    return (
-      <div className={jcss('mzd-tooltip-root', styles.tooltip_root)}>
-        <span className={jcss('mzd-tooltip-plate', styles.tooltip_plate)}>
-          {this.props.tooltip}
-        </span>
-        {this.props.children}
-      </div>
-    )
-  }
-}
+export const HoverTooltip = React.forwardRef(({ tooltip, children }, ref) => {
+  return (
+    <div className={'mzd-tooltip-root'} ref={ref}>
+      <span className={'mzd-tooltip-plate'}>{tooltip}</span>
+      {children}
+    </div>
+  )
+})
