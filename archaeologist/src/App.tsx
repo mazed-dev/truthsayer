105d6b26ae4929054b4da1b47a6ac4414dc92c70
akindyakov <akindyakov@gmail.com>
2021-11-16 08:59:30 +0000

    Add support for [emotion]

--- archaeologist/src/App.tsx
+++ archaeologist/src/App.tsx
@@ -1,16 +1,36 @@
+/** @jsxImportSource @emotion/react */
+
 import React from 'react'
-import { SaveButton } from './components/Button/Button'
+import {SaveButton} from './components/Button/Button'
 import * as log from './util/log'
 
-import { authCookie } from 'smuggler-api'
+import styled from '@emotion/styled'
+import {css} from '@emotion/react'
 
+const AppContainer = styled.div`
+  osition: relative;
+  width: 280px;
+  height: 280px;
+`
 export const App = () => {
   const [authenticated, setAuthenticated] = React.useState(false)
   log.debug('Render app', authenticated)
   return (
-    <>
+    <AppContainer>
       <SaveButton />
-    </>
+      <div
+        css={{
+          color: 'darkorchid',
+          '& .name': {
+            color: 'orange'
+          }
+        }}
+      >
+        This is darkorchid.
+              <div className="name">This is orange</div>
+
+      </div>
+    </AppContainer>
   )
 }
 
