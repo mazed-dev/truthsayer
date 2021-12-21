5edc818b508eb94c6ecd2692b77fd7848169b7a4
akindyakov <akindyakov@gmail.com>
2021-12-21 18:48:35 +0000

    Remove commented code

--- archaeologist/src/test/customEnvironment.js
+++ archaeologist/src/test/customEnvironment.js
@@ -12,15 +12,3 @@ module.exports = class CustomTestEnvironment extends Environment {
     }
   }
 }
-// const NodeEnvironment = require('jest-environment-node');
-
-// A custom environment to set the TextEncoder that is required by mongodb.
-// module.exports = class CustomTestEnvironment extends NodeEnvironment {
-//   async setup() {
-//       await super.setup();
-//           if (typeof this.global.TextEncoder === 'undefined') {
-//                 const { TextEncoder } = require('util');
-//                       this.global.TextEncoder = TextEncoder;
-//                           }
-//                             }
-//                             }
