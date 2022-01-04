3e5c28993833cdf0d79478b309b19651dba1262d
akindyakov <akindyakov@gmail.com>
2022-01-04 10:21:25 +0000

    Fix styles

--- elementary/src/colour.ts
+++ elementary/src/colour.ts
@@ -7,15 +7,15 @@ export enum Colour {
 }
 
 export const StyleButtonCreate = {
-    backgroundColor: Colour.ButtonCreate4,
-    borderColor: Colour.ButtonCreate4,
-    color: Colour.White,
-    '&:active': {
-      backgroundColor: Colour.ButtonCreate2,
-      borderColor: Colour.ButtonCreate1,
-    },
-    '&:hover': {
-      backgroundColor: Colour.ButtonCreate3,
-      borderColor: Colour.ButtonCreate2,
-    },
-  }
+  backgroundColor: Colour.ButtonCreate4,
+  borderColor: Colour.ButtonCreate4,
+  color: Colour.White,
+  '&:active': {
+    backgroundColor: Colour.ButtonCreate2,
+    borderColor: Colour.ButtonCreate1,
+  },
+  '&:hover': {
+    backgroundColor: Colour.ButtonCreate3,
+    borderColor: Colour.ButtonCreate2,
+  },
+}
