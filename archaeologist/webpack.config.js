17aa8f1d80a50f8772083b61d8439fdcf56e6720
akindyakov <akindyakov@gmail.com>
2022-01-07 10:27:35 +0000

    Downgrade manifest version to v3 at build time for firefox

--- archaeologist/webpack.config.js
+++ archaeologist/webpack.config.js
@@ -8,8 +8,29 @@ const _getSmugglerApiUrl = (mode) => {
     : "https://mazed.dev/smuggler"
 }
 
-const _manifestTransform = (buffer, mode) => {
-  const manifest = JSON.parse(buffer.toString());
+const _manifestTransformDowngradeToV2 = (manifest) => {
+  manifest.manifest_version = 2
+  // Is supported in V3 only
+  delete manifest.action
+  // background scripts are declared differently in v2
+  const { service_worker } = manifest.background
+  manifest.background.scripts = [ service_worker ]
+  delete manifest.background.service_worker
+  // host_permissions are not supported in v2
+  const {host_permissions} = manifest
+  manifest.permissions.push(...host_permissions)
+  manifest.permissions.push('<all_urls>')
+  delete manifest.host_permissions
+  // Manifest V3 new features
+  delete manifest.cross_origin_embedder_policy
+  delete manifest.cross_origin_opener_policy
+  return manifest
+}
+
+const _manifestTransform = (buffer, mode, env) => {
+  let manifest = JSON.parse(buffer.toString())
+
+  const {firefox=false, chrome=false, safari=false, edge=false} = env
 
   // Add Mazed URL to host_permissions to grant access to mazed cookies
   const mazedUrl = new URL(_getSmugglerApiUrl(mode))
@@ -17,7 +38,6 @@ const _manifestTransform = (buffer, mode) => {
   manifest["host_permissions"] = [
     mazedUrl.toString(),
   ]
-
   // Exclude Mazed from list of URLs where content.js is injected to
   mazedUrl.pathname = '*'
   manifest["content_scripts"].forEach((item, index, theArray) => {
@@ -30,6 +50,9 @@ const _manifestTransform = (buffer, mode) => {
       exclude_matches,
     }
   });
+  if (firefox) {
+    manifest = _manifestTransformDowngradeToV2(manifest)
+  }
   return JSON.stringify(manifest, null, 2);
 }
 
@@ -105,7 +128,7 @@ const config = (env, argv) => {
           from: "public", to: ".",
           transform: (context, absoluteFrom) => {
             if (absoluteFrom.endsWith("/manifest.json")) {
-              return _manifestTransform(context, argv.mode)
+              return _manifestTransform(context, argv.mode, env)
             }
             return context
           },
