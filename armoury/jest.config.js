b74c7eb2a4043895480822bfbfd13e9e9897a465
Sergey Nikitin <snikitin@outlook.com>
2022-02-19 18:15:45 +0000

    mv originId.ts to armoury (#160)

--- armoury/jest.config.js
+++ armoury/jest.config.js
@@ -1,20 +1,21 @@
 module.exports = {
-
   // The root of your source code, typically /src
   // `<rootDir>` is a token Jest substitutes
-  roots: ["<rootDir>/src"],
+  roots: ['<rootDir>/src', '<rootDir>/test'],
 
   // Jest transformations -- this adds support for TypeScript
   // using ts-jest
   transform: {
-    "^.+\\.tsx?$": "ts-jest"
+    '^.+\\.[tj]sx?$': 'ts-jest',
   },
+
+  transformIgnorePatterns: ['/node_modules/(?!normalize-url/)'],
 
   // Test spec file resolution pattern
   // Matches parent folder `__tests__` and filename
   // should contain `test` or `spec`.
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
 
   // Module file extensions for importing
-  moduleFileExtensions: ["ts", "js", "json", "node"]
-};
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+}
