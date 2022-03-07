07d24a3af5951191c281c81bcf78318a6a2751ed
akindyakov <akindyakov@gmail.com>
2022-03-07 16:43:11 +0000

    Remove dedicated zoom-in button under image node - use onImageClick instead with certain type of cursor

--- truthsayer/src/doc/image/ImageNode.tsx
+++ truthsayer/src/doc/image/ImageNode.tsx
@@ -1,6 +1,6 @@
-// @ts-nocheck
+/** @jsxImportSource @emotion/react */
 
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 
 import { Image, ButtonGroup, Modal } from 'react-bootstrap'
 
@@ -25,47 +25,40 @@ export const ImageNode = ({
   const source = node.getBlobSource()
   const [show, setShow] = useState(false)
 
-  let imageRef
-  const setImageRef = (element) => {
-    imageRef = element
-  }
-
+  const imageRef = useRef<HTMLImageElement>(null)
   const handleZoomIn = () => {
-    if (imageRef) {
-      const newMaxWidth = imageRef.offsetWidth * 1.1
-      imageRef.style.maxWidth = `${newMaxWidth}px`
+    const current = imageRef?.current
+    if (current != null) {
+      const newMaxWidth = current.offsetWidth * 1.1
+      current.style.maxWidth = `${newMaxWidth}px`
     }
   }
 
   const handleZoomOut = () => {
-    if (imageRef) {
-      const newMaxWidth = imageRef.offsetWidth * 0.9091
-      imageRef.style.maxWidth = `${newMaxWidth}px`
+    const current = imageRef?.current
+    if (current != null) {
+      const newMaxWidth = current.offsetWidth * 0.9091
+      current.style.maxWidth = `${newMaxWidth}px`
     }
   }
 
   const handleZoomReset = () => {
-    if (imageRef) {
-      imageRef.style.maxWidth = '100%'
+    const current = imageRef?.current
+    if (current != null) {
+      current.style.maxWidth = '100%'
     }
   }
 
+  if (source == null) {
+    return null
+  }
   return (
     <div className={className}>
       <Image
         src={source}
         className={jcss(styles.image, styles.image_in_card)}
-        ref={setImageRef}
+        onClick={() => setShow(true)}
       />
-      <div className={styles.buttons_under_image}>
-        <ImgButton onClick={() => setShow(true)}>
-          <img
-            src={ZoomInImg}
-            className={styles.zoom_button_image}
-            alt="Zoom image"
-          />
-        </ImgButton>
-      </div>
       <Modal show={show} fullscreen scrollable onHide={() => setShow(false)}>
         <Modal.Header closeButton>
           <Modal.Title className={styles.zoom_image_title}>
@@ -99,7 +92,7 @@ export const ImageNode = ({
           <Image
             src={source}
             className={jcss(styles.image, styles.image_full)}
-            ref={setImageRef}
+            ref={imageRef}
           />
         </Modal.Body>
       </Modal>
