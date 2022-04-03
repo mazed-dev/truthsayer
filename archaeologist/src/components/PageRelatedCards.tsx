46def16228c4db0acf10c074d120c5c02c245bee
akindyakov <akindyakov@gmail.com>
2022-04-03 14:52:34 +0100

    Next

--- archaeologist/src/components/PageRelatedCards.tsx
+++ archaeologist/src/components/PageRelatedCards.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import browser from 'webextension-polyfill'
 import styled from '@emotion/styled'
 
-import { TNode } from 'smuggler-api'
+import {TNode} from 'smuggler-api'
 
-import { mazed } from '../util/mazed'
-import { kSmallCardWidth } from 'elementary'
-import { NodeCard } from './NodeCard'
+import {mazed} from '../util/mazed'
+import {kSmallCardWidth} from 'elementary'
+import {NodeCard} from './NodeCard'
 
 const Box = styled.div`
   margin: 12px auto 0 auto;
@@ -15,15 +15,23 @@ const Box = styled.div`
   display: block;
 `
 
-export const PageRelatedCards = ({ node }: { node: TNode }) => {
-  const handleGoToNode = () => {
-    browser.tabs.create({
-      url: mazed.makeNodeUrl(node.nid).toString(),
-    })
-  }
+export const PageRelatedCards = ({bookmark, quotes}: {
+  bookmark: TNode | undefined
+  quotes: TNode[]
+}) => {
+  const refs = quotes.map((node: TNode) => {
+    return <NodeCard onClick={() => {}} node={node} key={node.nid} />
+  })
+  const bookmarkElement = bookmark == null ? null : (
+    <NodeCard onClick={() => {
+      browser.tabs.create({
+        url: mazed.makeNodeUrl(bookmark.nid).toString(),
+      })
+    }} node={bookmark} key={bookmark.nid} />)
   return (
     <Box>
-      <NodeCard onClick={handleGoToNode} node={node} />
+      {bookmarkElement}
+      {refs}
     </Box>
   )
 }
--- archaeologist/src/components/ViewActiveTabStatus.tsx
+++ archaeologist/src/components/ViewActiveTabStatus.tsx
@@ -1,17 +1,17 @@
 /** @jsxImportSource @emotion/react */
 
 import React from 'react'
-import { useAsyncEffect } from 'use-async-effect'
+import {useAsyncEffect} from 'use-async-effect'
 import browser from 'webextension-polyfill'
 import styled from '@emotion/styled'
 
-import { TNode } from 'smuggler-api'
+import {TNode} from 'smuggler-api'
 
-import { MdiBookmarkAdd, Spinner } from 'elementary'
-import { ButtonCreate } from './Button'
+import {MdiBookmarkAdd, Spinner} from 'elementary'
+import {ButtonCreate} from './Button'
 
-import { MessageType } from './../message/types'
-import { PageRelatedCards } from './PageRelatedCards'
+import {MessageType} from './../message/types'
+import {PageRelatedCards} from './PageRelatedCards'
 
 const Container = styled.div`
   margin: 0;
@@ -31,37 +31,42 @@ export const ViewActiveTabStatus = () => {
   const [pageStatus, setPageStatus] = React.useState<
     'saved' | 'loading' | 'unmemorable' | 'memorable'
   >('loading')
-  const [pageSavedNode, setPageSavedNode] = React.useState<any | null>(null)
+  const [pageSavedNode, setPageSavedNode] = React.useState<TNode | null>(null)
+  const [pageSavedQuotes, setPageSavedQuotes] = React.useState<TNode[]>([])
 
   useAsyncEffect(async () => {
     browser.runtime.onMessage.addListener((message: MessageType) => {
       switch (message.type) {
         case 'SAVED_NODE':
-          const { bookmark, unmemorable } = message
+          const {bookmark, quotes, unmemorable} = message
           if (bookmark != null) {
-            setPageSavedNode(bookmark)
+            const node = TNode.fromJson(bookmark)
+            setPageSavedNode(node)
             setPageStatus('saved')
           } else if (unmemorable) {
             setPageStatus('unmemorable')
           } else {
             setPageStatus('memorable')
           }
+          if (quotes) {
+            setPageSavedQuotes(quotes.map((json) => TNode.fromJson(json)))
+          }
           break
         default:
           break
       }
     })
-    await browser.runtime.sendMessage({ type: 'REQUEST_SAVED_NODE' })
+    await browser.runtime.sendMessage({type: 'REQUEST_SAVED_NODE'})
   }, [])
 
   const handleSave = () => {
     setPageStatus('loading')
-    browser.runtime.sendMessage({ type: 'REQUEST_PAGE_TO_SAVE' })
+    browser.runtime.sendMessage({type: 'REQUEST_PAGE_TO_SAVE'})
   }
 
   let btn
   let grid
-  if (pageStatus === 'memorable') {
+  if (pageStatus === 'memorable' && pageSavedNode == null) {
     btn = (
       <ButtonCreate onClick={handleSave}>
         <MdiBookmarkAdd
@@ -73,23 +78,11 @@ export const ViewActiveTabStatus = () => {
     )
   } else if (pageStatus === 'loading') {
     btn = <Spinner.Wheel />
-  } else if (pageStatus === 'unmemorable') {
-    btn = (
-      <div>
-        <p>This page can not be bookmarked, sorry for the inconvenience.</p>
-      </div>
-    )
-  } else if (pageStatus === 'saved') {
-    if (pageSavedNode != null) {
-      const node = TNode.fromJson(pageSavedNode)
-      grid = <PageRelatedCards node={node} />
-    } else {
-      btn = (
-        <div>
-          <p>Internal error</p>
-        </div>
-      )
-    }
+  } else {
+    grid = (<PageRelatedCards 
+            bookmark={pageSavedNode || undefined} 
+            quotes={pageSavedQuotes} 
+            />)
   }
   return (
     <Container>
