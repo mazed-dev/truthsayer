6f1ed79cfb6da7edfdb3eb3a6415f02cc8ce06fc
Alexander <akindyakov@gmail.com>
2022-05-13 09:02:17 +0100

    Replace [Type // to start...] with [Add a note...] (#210)
    - Replace [Type // to start...] with [Add a note...]
    - Show this tip only for empty document (for an only paragraph of the doc and only when it's empty)
    - Make tip invisible for clicks (with css attribute pointer-events: none)
    - Remove code that TS is complaining about

--- elementary/src/editor/components/Paragraph.tsx
+++ elementary/src/editor/components/Paragraph.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from '@emotion/styled'
-import { useSelected } from 'slate-react'
 import { ParagraphBox } from './components'
 
 import lodash from 'lodash'
@@ -9,9 +8,17 @@ const TipBox = styled.span`
   position: absolute;
   opacity: 0.32;
   margin-left: 2px;
+
   &:after {
-    content: 'Type // to insert...';
+    content: 'Add a note...';
     cursor: text;
+  }
+
+  pointer-events: none;
+
+  display: none;
+  p:only-of-type & {
+    display: inline;
   }
 `
 
@@ -21,12 +28,12 @@ type ParagraphProps = React.PropsWithChildren<{
 
 export const Paragraph = React.forwardRef<HTMLParagraphElement, ParagraphProps>(
   ({ className, children, ...attributes }, ref) => {
-    const selected = useSelected()
     let tip
-    // @ts-ignore: Property 'length' does not exist on type 'ReactNode'
-    if (selected && children?.length === 1) {
-      // @ts-ignore: expression of type '0' can't be used to index type 'ReactNode'
-      const text = lodash.get(children[0], 'props.text.text')
+    if (React.Children.count(children) === 1) {
+      const text = lodash.get(
+        React.Children.toArray(children)[0],
+        'props.text.text'
+      )
       if (text === '') {
         tip = <TipBox contentEditable={false} />
       }
