f4c714e53e6215bcfb31cc2cd1e22c03a906a55f
Alexander <akindyakov@gmail.com>
2022-06-17 10:32:14 +0100

    Always add XML markup for Chrome to avoid runtime errors (#225)

--- armoury/src/log.ts
+++ armoury/src/log.ts
@@ -3,6 +3,19 @@ export const log = {
     console.log('Debug', ...args) // eslint-disable-line no-console
   },
 
+  /**
+   * Funcional style logging
+   * to inject logging in functions call chain:
+   *  Before:
+   *    teleportBar(fooToBar(createFoo()))
+   *  After:
+   *    teleportBar(log.fdebug(fooToBar(log.fdebug(createFoo()))))
+   */
+  fdebug<T>(v: T, ...args: any): T {
+    console.log('Debug()', v, ...args) // eslint-disable-line no-console
+    return v
+  },
+
   exception(err: Error, ...args: any): void {
     console.error(err, ...args) // eslint-disable-line no-console
   },
