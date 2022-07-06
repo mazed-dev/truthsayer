fa6f4564bc8b79ab13ef9497ecac3c4af100416f
akindyakov <akindyakov@gmail.com>
2022-07-06 10:49:57 +0100

    Linter

--- archaeologist/src/content/toaster/Toaster.tsx
+++ archaeologist/src/content/toaster/Toaster.tsx
@@ -16,25 +16,23 @@ export const Toaster = ({ children }: React.PropsWithChildren<{}>) => {
     'mazed-archaeologist-toaster-container'
   )
   /**
-  * This is an effect to inject element container to the content DOM tree. There
-  * are 2 reasons to use `useEffect` for it:
-  *
-  *   1. We need a clean up (see lambda as a return value of `useEffect`).
-  *   Otherwise react would never delete rendered element.
-  *   2. Container has to be deleted-and-added __on every render__, otherwise we
-  *   would see only first version of the element without any further updates.
-  *   There is no dependency list here on purpose, to re-inject container into
-  *   the content DOM on every update.
-  */
-  React.useEffect(
-    () => {
-      const target = document.body
-      target.appendChild(container)
-      return () => {
-        target.removeChild(container)
-      }
+   * This is an effect to inject element container to the content DOM tree. There
+   * are 2 reasons to use `useEffect` for it:
+   *
+   *   1. We need a clean up (see lambda as a return value of `useEffect`).
+   *   Otherwise react would never delete rendered element.
+   *   2. Container has to be deleted-and-added __on every render__, otherwise we
+   *   would see only first version of the element without any further updates.
+   *   There is no dependency list here on purpose, to re-inject container into
+   *   the content DOM on every update.
+   */
+  React.useEffect(() => {
+    const target = document.body
+    target.appendChild(container)
+    return () => {
+      target.removeChild(container)
     }
-  )
+  })
   return ReactDOM.createPortal(
     <ToasterBox id={kToasterBoxElementId}>{children}</ToasterBox>,
     container
@@ -74,7 +72,10 @@ export const Toast = ({
      * the box do not get through.
      */
   )
-  return ReactDOM.createPortal(<ToastBox key={toastKey}>{children}</ToastBox>, box)
+  return ReactDOM.createPortal(
+    <ToastBox key={toastKey}>{children}</ToastBox>,
+    box
+  )
 }
 
 export type DisappearingToastProps = {
