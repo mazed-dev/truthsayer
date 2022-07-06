9531605c747afed410f31b7b996244d1a5226a3a
akindyakov <akindyakov@gmail.com>
2022-07-06 10:45:06 +0100

    Comment the usage of effect to (re)injecting the container with toaster

--- archaeologist/src/content/toaster/Toaster.tsx
+++ archaeologist/src/content/toaster/Toaster.tsx
@@ -15,6 +15,17 @@ export const Toaster = ({ children }: React.PropsWithChildren<{}>) => {
   const container = document.createElement(
     'mazed-archaeologist-toaster-container'
   )
+  /**
+  * This is an effect to inject element container to the content DOM tree. There
+  * are 2 reasons to use `useEffect` for it:
+  *
+  *   1. We need a clean up (see lambda as a return value of `useEffect`).
+  *   Otherwise react would never delete rendered element.
+  *   2. Container has to be deleted-and-added __on every render__, otherwise we
+  *   would see only first version of the element without any further updates.
+  *   There is no dependency list here on purpose, to re-inject container into
+  *   the content DOM on every update.
+  */
   React.useEffect(
     () => {
       const target = document.body
@@ -23,10 +34,6 @@ export const Toaster = ({ children }: React.PropsWithChildren<{}>) => {
         target.removeChild(container)
       }
     }
-    /**
-     * There is no dependency list here on purpose, otherwise content updates to
-     * the container do not get through.
-     */
   )
   return ReactDOM.createPortal(
     <ToasterBox id={kToasterBoxElementId}>{children}</ToasterBox>,
