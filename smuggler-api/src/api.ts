37be2b6ab12f3cc85e1d74abd3bd16e79842e7fa
akindyakov <akindyakov@gmail.com>
2022-07-18 11:56:49 +0100

    Use function to create an  from

--- smuggler-api/src/api.ts
+++ smuggler-api/src/api.ts
@@ -119,7 +119,7 @@ async function createNode(
   if (resp.ok) {
     return await resp.json()
   }
-  throw new Error(`(${resp.status}) ${resp.statusText}`)
+  throw _makeResponseError(resp)
 }
 
 function lookupKeyOf(args: CreateNodeArgs): NodeLookupKey | undefined {
@@ -167,9 +167,7 @@ async function createOrUpdateNode(
 
   const resp = await updateNode(updateArgs, signal)
   if (!resp.ok) {
-    throw new Error(
-      `Failed to update node ${existingNode.nid} (${resp.status}) ${resp.statusText}`
-    )
+    throw _makeResponseError(resp, `Failed to update node ${existingNode.nid}`)
   }
   return {
     nid: existingNode.nid,
@@ -329,7 +327,7 @@ async function uploadFiles(
     signal,
   })
   if (!resp.ok) {
-    throw new Error(`(${resp.status}) ${resp.statusText}`)
+    throw _makeResponseError(resp)
   }
   return await resp.json()
 }
@@ -348,7 +346,7 @@ async function buildFilesSearchIndex(
     signal,
   })
   if (!resp.ok) {
-    throw new Error(`(${resp.status}) ${resp.statusText}`)
+    throw _makeResponseError(resp)
   }
   return await resp.json()
 }
@@ -376,7 +374,7 @@ async function deleteNode({
   if (resp.ok) {
     return await resp.json()
   }
-  throw new Error(`(${resp.status}) ${resp.statusText}`)
+  throw _makeResponseError(resp)
 }
 
 async function getNode({
@@ -388,7 +386,7 @@ async function getNode({
 }): Promise<TNode> {
   const res = await fetch(makeUrl(`/node/${nid}`), { method: 'GET', signal })
   if (!res.ok) {
-    throw new Error(`(${res.status}) ${res.statusText}`)
+    throw _makeResponseError(res)
   }
   const {
     text,
@@ -445,7 +443,10 @@ async function getAuth({
 }): Promise<AccountInfo> {
   const resp = await fetch(makeUrl('/auth'), { method: 'GET', signal })
   if (!resp.ok) {
-    throw new Error('Retrieving account information failed with an error')
+    throw _makeResponseError(
+      resp,
+      'Retrieving account information failed with an error'
+    )
   }
   return await resp.json()
 }
@@ -483,7 +484,7 @@ export const getNodesSlice: GetNodesSliceFn = async ({
     signal,
   })
   if (!rawResp.ok) {
-    throw new Error(`(${rawResp.status}) ${rawResp.statusText}`)
+    throw _makeResponseError(rawResp)
   }
   const response: NodeAttrsSearchResponse = await rawResp.json()
   const nodes = response.nodes.map((item) => {
@@ -565,7 +566,7 @@ async function createEdge({
     signal,
   })
   if (!resp.ok) {
-    throw new Error('Empty edge creation response')
+    throw _makeResponseError(resp, 'Empty edge creation response')
   }
   const { edges } = await resp.json()
   if (!edges?.length) {
@@ -587,7 +588,7 @@ async function getNodeEdges(
     signal,
   })
   if (!resp.ok) {
-    throw new Error('Getting node edges failed with error')
+    throw _makeResponseError(resp, 'Getting node edges failed with error')
   }
   const star = await resp.json()
   star.edges = star.edges.map((edgeObj: EdgeAttributes) => {
@@ -638,7 +639,10 @@ async function switchEdgeStickiness({
     signal,
   })
   if (!resp.ok) {
-    throw new Error('Switching edge stickiness failed with error')
+    throw _makeResponseError(
+      resp,
+      'Switching edge stickiness failed with error'
+    )
   }
   return await resp.json()
 }
@@ -661,7 +665,7 @@ async function deleteEdge({
   if (resp.ok) {
     return await resp.json()
   }
-  throw new Error(`(${resp.status}) ${resp.statusText}`)
+  throw _makeResponseError(resp)
 }
 
 function verifyIsNotNull(value: Optional<any>): void {
@@ -694,7 +698,8 @@ async function createSession(
     signal,
   })
   if (!resp.ok) {
-    throw new Error(
+    throw _makeResponseError(
+      resp,
       `Session creation failed with an error (${resp.status}) ${resp.statusText}`
     )
   }
@@ -709,7 +714,7 @@ async function deleteSession({ signal }: { signal: AbortSignal }) {
   if (resp.ok) {
     return await resp.json()
   }
-  throw new Error(`(${resp.status}) ${resp.statusText}`)
+  throw _makeResponseError(resp)
 }
 
 async function updateSession(signal?: AbortSignal): Promise<Ack> {
@@ -720,7 +725,7 @@ async function updateSession(signal?: AbortSignal): Promise<Ack> {
   if (resp.ok) {
     return await resp.json()
   }
-  throw new Error(`(${resp.status}) ${resp.statusText}`)
+  throw _makeResponseError(resp)
 }
 
 function _makeExternalUserActivityUrl(origin: OriginId): string {
@@ -793,7 +798,7 @@ async function getUserBadge({
   if (resp.ok) {
     return await resp.json()
   }
-  throw new Error(`(${resp.status}) ${resp.statusText}`)
+  throw _makeResponseError(resp)
 }
 
 async function registerAccount({
@@ -815,7 +820,7 @@ async function registerAccount({
   if (resp.ok) {
     return await resp.json()
   }
-  throw new Error(`(${resp.status}) ${resp.statusText}`)
+  throw _makeResponseError(resp)
 }
 
 async function passwordReset({
@@ -837,7 +842,7 @@ async function passwordReset({
   if (resp.ok) {
     return await resp.json()
   }
-  throw new Error(`(${resp.status}) ${resp.statusText}`)
+  throw _makeResponseError(resp)
 }
 
 async function passwordRecoverRequest({
@@ -857,7 +862,7 @@ async function passwordRecoverRequest({
   if (resp.ok) {
     return await resp.json()
   }
-  throw new Error(`(${resp.status}) ${resp.statusText}`)
+  throw _makeResponseError(resp)
 }
 
 async function passwordChange(
@@ -875,7 +880,7 @@ async function passwordChange(
   if (resp.ok) {
     return await resp.json()
   }
-  throw new Error(`(${resp.status}) ${resp.statusText}`)
+  throw _makeResponseError(resp)
 }
 
 async function getUserFsIngestionProgress(
@@ -892,9 +897,9 @@ async function getUserFsIngestionProgress(
   if (resp.ok) {
     return await resp.json()
   }
-  throw new Error(
-    `Failed to get ingestion progress for ${JSON.stringify(fsid)}` +
-      ` (${resp.status}) ${resp.statusText}`
+  throw _makeResponseError(
+    resp,
+    `Failed to get ingestion progress for ${JSON.stringify(fsid)}`
   )
 }
 async function advanceUserFsIngestionProgress(
@@ -914,14 +919,14 @@ async function advanceUserFsIngestionProgress(
   if (resp.ok) {
     return await resp.json()
   }
-  throw new Error(
-    `Failed to advance ingestion progress for ${JSON.stringify(fsid)}` +
-      ` (${resp.status}) ${resp.statusText}`
+  throw _makeResponseError(
+    resp,
+    `Failed to advance ingestion progress for ${JSON.stringify(fsid)}`
   )
 }
 
 function _makeResponseError(response: Response, message?: string): Error {
-  return new Error(`${message} (${response.status}) ${response.statusText}`)
+  return new Error(`${message} ([${response.status}] ${response.statusText})`)
 }
 
 export const smuggler = {
