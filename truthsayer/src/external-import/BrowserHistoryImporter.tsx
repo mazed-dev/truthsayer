aa0b8568338dcda1ff7d42de3aec67e5cd47a0bf
Sergey Nikitin <snikitin@outlook.com>
2022-10-22 18:41:56 +0100

    always infer archaeologist version from manifest.json (#308)

--- truthsayer/src/external-import/BrowserHistoryImporter.tsx
+++ truthsayer/src/external-import/BrowserHistoryImporter.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import semver from 'semver'
 
 import { log, sleep } from 'armoury'
 import {
@@ -14,46 +15,84 @@ export { BrowserLogo }
 
 const Box = styled.div``
 
-const kExpectedArchaeologistVersion = '0.1.16'
+const kMinimalArchaeologistVersion = '0.1.16'
 
-type ArchaeologistState = 'not-found' | 'loading' | 'good'
+type ArchaeologistState =
+  | { type: 'not-found' }
+  | { type: 'version-mismatch'; actual: string }
+  | { type: 'loading' }
+  | { type: 'good' }
 export function BrowserHistoryImporter({ className }: { className?: string }) {
   const [archaeologistState, setArchaeologistState] =
-    React.useState<ArchaeologistState>('loading')
+    React.useState<ArchaeologistState>({ type: 'loading' })
   React.useEffect(() => {
     const id: truthsayer_archaeologist_communication.VersionId =
       'mazed-archaeologist-version'
     sleep(250).then(() => {
       const el = window.document.getElementById(id)
+      if (el == null) {
+        setArchaeologistState({ type: 'not-found' })
+        return
+      }
+      let version: truthsayer_archaeologist_communication.VersionStruct | null =
+        null
       try {
-        if (el != null) {
-          const version = JSON.parse(
-            el.innerHTML
-          ) as truthsayer_archaeologist_communication.VersionStruct
-          if (kExpectedArchaeologistVersion === version.version) {
-            setArchaeologistState('good')
-          } else {
-            setArchaeologistState('not-found')
-          }
-        }
+        version = JSON.parse(
+          el.innerHTML
+        ) as truthsayer_archaeologist_communication.VersionStruct
       } catch (err) {
         log.error('Archaeologist version deserialization failed with', err)
+        return
       }
+      const state: ArchaeologistState = semver.gte(
+        version.version,
+        kMinimalArchaeologistVersion
+      )
+        ? { type: 'good' }
+        : {
+            type: 'version-mismatch',
+            actual: version.version,
+          }
+      setArchaeologistState(state)
     })
   }, [])
+
   return (
     <Box className={className}>
       <TruthsayerBrowserHistoryImportWidget />
-      {archaeologistState === 'loading' ? (
-        <Spinner.Wheel width={32} />
-      ) : archaeologistState === 'not-found' ? (
+      {describe(archaeologistState)}
+    </Box>
+  )
+}
+
+function describe(state: ArchaeologistState) {
+  switch (state.type) {
+    case 'loading': {
+      return <Spinner.Wheel width={32} />
+    }
+    case 'not-found': {
+      return (
         <div>
           Compatible version of Mazed browser extension is not found, go to{' '}
           <TruthsayerLink to={'/apps-to-install'}>Mazed Apps</TruthsayerLink> to
-          install Mazed for your browser. Expected version of browser extension
-          is &ldquo;{kExpectedArchaeologistVersion}&rdquo;.
+          install Mazed for your browser. Minimal version of browser extension
+          is &ldquo;{kMinimalArchaeologistVersion}&rdquo;.
         </div>
-      ) : null}
-    </Box>
-  )
+      )
+    }
+    case 'version-mismatch': {
+      return (
+        <div>
+          Mazed browser extension is of version &ldquo;{state.actual}&rdquo;,
+          minimal required version is &ldquo;
+          {kMinimalArchaeologistVersion}&rdquo;. Ensure you have the latest
+          version via{' '}
+          <TruthsayerLink to={'/apps-to-install'}>Mazed Apps</TruthsayerLink>.
+        </div>
+      )
+    }
+    case 'good': {
+      return null
+    }
+  }
 }
