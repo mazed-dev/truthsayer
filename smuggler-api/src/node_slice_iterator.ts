b550219d904c2a1043530949b48585c2cd4edab0
Sergey Nikitin <snikitin@outlook.com>
2023-01-14 16:38:15 +0000

    implement StorageApi for browser.storage.StorageArea (#396)

--- smuggler-api/src/node_slice_iterator.ts
+++ smuggler-api/src/node_slice_iterator.ts
@@ -4,7 +4,6 @@ import type { Optional } from 'armoury'
 export interface INodeIterator {
   next: () => Promise<Optional<TNode>>
   total: () => number
-  exhausted: () => boolean
   abort: () => void
 }
 
