cb0215126c1996efcd9451934d35c8847a1591ca
Alexander <akindyakov@gmail.com>
2023-02-09 12:38:07 +0000

    New landing page (#433)

--- truthsayer/src/landing-page/LandingPage.tsx
+++ truthsayer/src/landing-page/LandingPage.tsx
@@ -7,11 +7,16 @@ import { ThemeProvider } from '@emotion/react'
 import { Form } from 'react-bootstrap'
 
 import { goto, routes } from './../lib/route'
-import DemoQuoteImg from './img/copy-quote-demo.png'
-import DemoWritingAugmentationGif from './img/mazed-demo-calendar-writing-augmentation-safes.gif'
+import { getLogoImage } from './../util/env'
 
 import { authentication } from 'smuggler-api'
-import { log } from 'armoury'
+import { log, MimeType } from 'armoury'
+
+import {
+  Linkedin as LinkedinLogo,
+  Google as GoogleLogo,
+  Salesforce as SalesforceLogo,
+} from '@emotion-icons/fa-brands'
 
 const SlidesBox = styled.div`
   height: 100vh;
@@ -44,80 +49,83 @@ const FirstSlideBody = styled.div`
   height: 88vh;
 `
 const FirstSlideLeftHalf = styled.div`
-  width: 50%;
-  @media (max-width: 900px) {
-    width: 100%;
-  }
+  width: 100%;
 
   display: flex;
   justify-content: center;
   flex-wrap: nowrap;
   flex-direction: column;
 
-  padding: 42px;
-`
-const FirstSlideRightHalf = styled.div`
-  width: 50%;
-  @media (max-width: 900px) {
-    width: 0%;
-    display: none;
-  }
-  display: flex;
-  justify-content: center;
-  flex-wrap: nowrap;
-  flex-direction: column;
-`
-const SecondSlideBody = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  padding-top: 8vh;
-`
-const SecondSlideRightHalf = styled.div`
-  width: 50%;
-  @media (max-width: 900px) {
-    width: 0%;
-    display: none;
-  }
-
-  display: flex;
-  justify-content: center;
-  flex-wrap: nowrap;
-  flex-direction: column;
-`
-const SecondSlideLeftHalf = styled.div`
-  width: 50%;
-  @media (max-width: 900px) {
-    width: 100%;
-  }
-
-  display: flex;
-  justify-content: center;
-  flex-wrap: nowrap;
-  flex-direction: column;
-
-  padding: 42px;
+  padding: 20px;
 `
 const Header = styled.h1`
-  font-size: 32px;
+  font-size: 48px;
+  @media (max-width: 800px) {
+    font-size: 36px;
+  }
+  @media (max-width: 600px) {
+    font-size: 32px;
+  }
   text-align: center;
 `
 const Description = styled.h2`
-  font-size: 18px;
+  font-size: 28px;
+  @media (max-width: 800px) {
+    font-size: 20px;
+  }
+  @media (max-width: 600px) {
+    font-size: 16px;
+  }
   text-align: center;
-  margin: 3vh 12px 0 12px;
+  margin-top: 40px;
+`
+const Comment = styled.h2`
+  font-size: 14px;
+  @media (max-width: 800px) {
+    font-size: 13px;
+  }
+  @media (max-width: 600px) {
+    font-size: 12px;
+  }
+  text-align: center;
+  margin-top: 8vh;
+`
+const TrustedByBox = styled.div``
+const TrustedByTitle = styled(Comment)``
+const TrustedByLogosBox = styled.div`
+  font-size: 48px;
+  @media (max-width: 800px) {
+    font-size: 36px;
+  }
+  @media (max-width: 600px) {
+    font-size: 32px;
+  }
+  display: flex;
+  justify-content: center;
+  gap: 0.8em;
+  margin-top: 12px;
 `
 
 const Logo = styled.div`
   font-family: 'Comfortaa';
-  font-size: 32px;
-  cursor: pointer;
   font-weight: 900;
-
+  font-size: 32px;
+  @media (max-width: 800px) {
+    font-size: 28px;
+  }
+  cursor: pointer;
+  color: white;
   &:hover {
     border: none;
     background-color: ${(props) => props.theme.backgroundColor.primary};
-    color: ${(props) => props.theme.color.positive};
   }
+`
+
+const LogoImg = styled.img`
+  filter: invert(1);
+  width: 38px;
+  height: 38px;
+  margin-bottom: 8px;
 `
 const Topbar = styled.div`
   display: flex;
@@ -162,25 +170,19 @@ function Login() {
     </LoginBox>
   )
 }
-const GifDemo = styled.img`
-  width: 94%;
-  border-color: #cecece;
-  border-style: solid;
-  box-shadow: 1px 1px 4px ${(props) => props.theme.backgroundColor.negative};
-  filter: ${(props) => props.theme.image.filter};
-`
-const ImageDemo = styled.img`
-  height: 86vh;
-  border-color: #cecece;
-  border-style: solid;
-  box-shadow: 1px 1px 4px ${(props) => props.theme.backgroundColor.negative};
-  filter: ${(props) => props.theme.image.filter};
-`
+
+// const ImageDemo = styled.img`
+//   height: 86vh;
+//   border-color: #cecece;
+//   border-style: solid;
+//   box-shadow: 1px 1px 4px ${(props) => props.theme.backgroundColor.negative};
+//   filter: ${(props) => props.theme.image.filter};
+// `
+
 const SignUpFormBox = styled.form`
-  margin-top: 14vh;
   border-radius: 10px;
   width: 100%;
-  font-size: 17px;
+  font-size: 16px;
 
   display: flex;
   justify-content: center;
@@ -189,13 +191,17 @@ const SignUpFormBox = styled.form`
   @media (max-width: 480px) {
     flex-wrap: wrap;
   }
+  margin: 12px 0 0 0;
 `
 const SignUpBtn = styled.button`
   white-space: nowrap;
   cursor: pointer;
   font-size: inherit;
-  margin: 1em 2px 1px 1px;
-  padding: 0.32em 1em 0.32em 1em;
+  margin: 0 2px 1px 0;
+  padding: 0.32em 0.8em 0.32em 0.8em;
+  @media (max-width: 360px) {
+    padding: 0.32em 0.4em 0.32em 0.4em;
+  }
 
   background-color: ${(props) => props.theme.backgroundColor.primary};
   color: ${(props) => props.theme.color.primary};
@@ -203,6 +209,9 @@ const SignUpBtn = styled.button`
   border-color: ${(props) => props.theme.color.primary};
   border-style: solid;
   border-radius: inherit;
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+  border-left-width: 0;
 
   &:hover {
     background-color: ${(props) => props.theme.backgroundColor.primary};
@@ -211,10 +220,13 @@ const SignUpBtn = styled.button`
   }
 `
 const SignUpEmail = styled(Form.Control)`
-  width: 60%;
+  width: 54%;
+  @media (max-width: 360px) {
+    width: 42%;
+  }
   max-width: 18em;
   font-size: inherit;
-  margin: 1em 2px 1px 1px;
+  margin: 0 0 1px 1px;
 
   background-color: ${(props) => props.theme.backgroundColor.primary};
   color: ${(props) => props.theme.color.primary};
@@ -222,6 +234,8 @@ const SignUpEmail = styled(Form.Control)`
   border-style: solid;
   border-width: 1px;
   border-radius: inherit;
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
 
   &:focus {
     background-color: ${(props) => props.theme.backgroundColor.primary};
@@ -299,14 +313,14 @@ const FooterItem = styled.div`
   font-sixe: 16px;
 `
 
-const TextStrikeThrough = styled.span`
-  text-decoration: line-through;
-  text-decoration-color: ${(props) => props.theme.color.positive};
-`
-const TextHighlight = styled.span`
-  font-weight: 900;
-  color: ${(props) => props.theme.color.positive};
-`
+// const TextStrikeThrough = styled.span`
+//   text-decoration: line-through;
+//   text-decoration-color: ${(props) => props.theme.color.positive};
+// `
+// const TextHighlight = styled.span`
+//   font-weight: 900;
+//   color: ${(props) => props.theme.color.positive};
+// `
 
 const themeDark = {
   backgroundColor: {
@@ -330,50 +344,40 @@ export function LandingPage() {
       <SlidesBox>
         <Slide>
           <Topbar>
-            <Logo>🧵 Mazed</Logo>
+            <Logo>
+              <LogoImg src={getLogoImage(MimeType.IMAGE_SVG_XML)} /> Mazed
+            </Logo>
             <Login />
           </Topbar>
           <FirstSlideBody>
             <FirstSlideLeftHalf>
               <Header>
-                <div>Share any page you've read.</div>
                 <div>
-                  <b>Without searching for it.</b>
+                  Reference anything you've read. <wbr />
+                  <b>Without&nbsp;looking&nbsp;for&nbsp;it.</b>
                 </div>
               </Header>
               <Description>
-                Mazed is a browser extension that saves the pages you view
-                automatically, and resurfaces them to you when you need it. Your
-                memory, at your fingertips.
+                Mazed&nbsp;is&nbsp;your&nbsp;second&nbsp;brain, serving you
+                information you've read before, when&nbsp;you&nbsp;need&nbsp;it.
               </Description>
+              <Comment>
+                We&nbsp;are&nbsp;currently&nbsp;in&nbsp;a&nbsp;private&nbsp;beta.
+                Register&nbsp;your&nbsp;email and&nbsp;we'll&nbsp;ping&nbsp;you
+                when&nbsp;you're&nbsp;off&nbsp;the waitlist.
+              </Comment>
               <SignUpForm />
+              <TrustedByBox>
+                <TrustedByTitle>
+                  Trusted by engineers and their teams at these companies:
+                </TrustedByTitle>
+                <TrustedByLogosBox>
+                  <LinkedinLogo size="1em" /> <GoogleLogo size="1em" />{' '}
+                  <SalesforceLogo size="1em" />
+                </TrustedByLogosBox>
+              </TrustedByBox>
             </FirstSlideLeftHalf>
-            <FirstSlideRightHalf>
-              <Centered>
-                <GifDemo src={DemoWritingAugmentationGif} />
-              </Centered>
-            </FirstSlideRightHalf>
           </FirstSlideBody>
-        </Slide>
-        <Slide>
-          <SecondSlideBody>
-            <SecondSlideRightHalf>
-              <Centered>
-                <ImageDemo src={DemoQuoteImg} />
-              </Centered>
-            </SecondSlideRightHalf>
-            <SecondSlideLeftHalf>
-              <Header>
-                <TextStrikeThrough>Find </TextStrikeThrough>
-                <TextHighlight>Have</TextHighlight> what you need.
-              </Header>
-              <Description>
-                The article, page, or google doc you need is just a click away.
-                Mazed will suggest the relevant pages from your history to you,
-                as you type.
-              </Description>
-            </SecondSlideLeftHalf>
-          </SecondSlideBody>
         </Slide>
         <LastSlide>
           <Centered>
