870530d5a5aaf7be4d8b3120db915e15d7240ad7
akindyakov <akindyakov@gmail.com>
2023-02-26 14:44:52 +0000

    Update PP to include local mode

--- truthsayer/src/public-page/legal/PrivacyPolicy.tsx
+++ truthsayer/src/public-page/legal/PrivacyPolicy.tsx
@@ -16,25 +16,36 @@ export function PrivacyPolicy() {
 const _md = `
 # Privacy Policy
 
-Mazed exists to give you the ability to make your memory instantly accessible to yourself. This could not be done without the trust of our users. A fundamental piece of earning that trust means being clear about how we use your information and protecting your human right to privacy.  This Privacy Policy explains the practices regarding the collection, use and disclosure of information received through the Services of ASG Mazed Inc. (“Mazed”, “we”, “us”, “our”). This Privacy Policy does not apply to any third-party websites, services or applications, even if they are accessible through our Services.
+**Updated 2023/02/26**
 
-Please note that, unless we define a term in this Privacy Policy, all capitalized terms used in this Privacy Policy have the same meanings as in our [Terms and Conditions](./terms-of-service). So, please make sure that you have read and understood our [Terms and Conditions](./terms-of-service).
 
-**Introduction**
+## Introduction
 
-This Privacy Policy applies to both registered **“Members”** and unregistered **“Visitors”** for use of the Services, and any individual or entity that uses the Services (as defined below), who are all hereinafter referred to as **“User(s)”**.  By accessing or using our Services, the User is entering into a legally binding agreement (either individually or on behalf of a Company) that includes the Mazed [Terms and Conditions](./terms-of-service). and this Privacy Policy.
+Mazed exists to allow Users to instantly access and use their memory. This could not be done without the trust of our users. A fundamental piece of earning that trust means being clear about how we use your information and protecting your human right to privacy by collecting the minimum amount of information necessary.
+
+This Privacy Policy explains the practices regarding the collection, use and disclosure of information received through the Services of ASG Mazed Inc. (“Mazed”, “we”, “us”, “our”). This Privacy Policy does not apply to any third-party websites, services or applications, even if they are accessible through our Services. Please note that, unless we define a term in this Privacy Policy, all capitalized terms used in this Privacy Policy have the same meanings as in our [Terms of Service](https://mazed.se/terms-of-service). So, please make sure that you have read and understood our [Terms of Service](https://mazed.se/terms-of-service).
+
+This Privacy Policy applies to both registered **“Members”** and unregistered **“Visitors”** for use of the Services, and any individual or entity that uses the Services (as defined below), who are all hereinafter referred to as **“User(s)”**.  By accessing or using our Services, the User is entering into a legally binding agreement (either individually or on behalf of a Company) that includes the Mazed [Terms and Conditions](https://mazed.se/terms-and-conditions) and this Privacy Policy.
 
 This Privacy Policy applies to all Users of mazed.se and all other Mazed websites, apps, developer platforms, management and transaction services, premium services, other products and services offered by Mazed, or any content or information provided as part of these services, including our blog (collectively, the **“Services”**).
 
 This Privacy Policy is effective upon use of any Mazed Services by all Users.
 
-**Types of Information Collected**
+
+## Types of Information Collected
 
 Mazed collects the minimum amount of information necessary to provide our services, and does so by storing User information locally on User devices. Only the information stated below is collected by Mazed on Mazed’s servers.
 
 We may collect certain anonymized information from **Visitors** to the mazed.se website, including your computer's Internet Protocol (IP) address, your browser type and version, your user agent, the pages you visit on the Mazed website, the time and date of your visit, the time spent on each page, and other details. This information is collected for the purpose of improving our website and providing you with a better User experience. We may also use this information to analyze User behavior, troubleshoot technical issues, and comply with legal requirements. By visiting the Mazed website, you consent to the collection and use of this information as described in this Privacy Policy.
 
 As a registered User of Mazed, we collect solely the information necessary to provide our services. The only personal information that we collect on our servers from Users is personal information for account registration, such as your name, email address, job title, and password; and any information you provide to us when contacting us through email or phone when contacting Mazed technical support, from here on all titled “Provided Data.” We collect this information to provide you with our services and to improve the User experience.
+
+
+### Cookies
+
+Cookies are small text files placed in device browsers to store their preferences. Mazed uses cookies for authorization when Users return to Mazed.se. Most browsers allow you to block and delete cookies. However, if you do that, the Services may not work properly.
+
+### Analytics
 
 We also collect anonymized usage data for analytics through [Google Analytics](https://analytics.google.com/) and [Posthog](https://posthog.com/) (“Service Providers''), such as network monitoring, clicks, mouse position, scroll, or keystroke logging. We may use this anonymized information to analyze User behavior, troubleshoot technical issues, and comply with legal requirements. By registering as a User of Mazed, you consent to the collection and use of this information as described in this Privacy Policy. Neither Mazed, nor our Service Providers, collect content or communications data of Users, such as messages, emails, or posts.
 
@@ -43,7 +54,7 @@ All other User data used by the Mazed web extension to provide its services, inc
 Personal information for authentication purposes and anonymized usage data is stored on our servers. Mazed uses servers that may be located anywhere in the world, including but not limited to, the United States or other countries, abiding by guidelines and regulations of relevant legal jurisdictions. If User has any concern about providing information to Mazed as detailed in this Privacy Policy, User should not become a Member, visit our sites, applications, or otherwise use our Services.
 
 
-## **Use of User's Data**
+## Use of User's Data
 
 Mazed collects this Provided Data in order to provide the User with a safe, smooth, efficient and customized experience. User understands, accepts, and consents that Mazed is authorized to use User’s personal information in the following ways:
 
@@ -51,27 +62,27 @@ Mazed is authorized to use the User’s Provided Data to:
 
 1. Provide the Services and client services support requested by User;
 2. Resolve disputes, collect Fees, and troubleshoot problems;
-3. Enforce the Mazed [Terms and Conditions](./terms-of-service) and this Privacy Policy;
+3. Enforce the Mazed [Terms and Conditions](https://mazed.se/terms-and-conditions) and this Privacy Policy;
 4. Customize, measure and improve its Services and content;
 5. Inform User about Service updates, changes, and promotions; and
 6. Compare information for accuracy, and to verify it with third parties.
 
 
-### **Communication **
+## Communication
 
 Mazed may communicate with its Users via telephone, email, notices posted on the Mazed sites or apps, direct messages, and other means available through the Services. Communications may include welcome and setup communications, Service communications, promotional communications, and other account support communications. These communications will be sent to User based on User’s account information and communication preferences. User understands and acknowledges, however, that User cannot opt out of receiving Service communications from Mazed.
 
 
-## **Data Modification and Retention**
+## Data Modification and Retention
 
 The User can see, review and change most of its personal information by signing into User’s Mazed account. Generally, Mazed will not manually modify User’s Provided Data because it is difficult to verify your identity remotely. Accordingly, we strongly encourage, and sometimes require, Users to update their Provided Data directly in their Mazed account if it changes or is inaccurate.
 
-Upon User’s written request, Mazed will close User’s account and remove User’s Provided Data from view as soon as reasonably practicable, based on User’s account activity and in accordance with applicable law. However, User acknowledges, understands and consents that Mazed may retain User’s Provided Data from closed accounts to comply with the law, prevent fraud, collect any fees owed, resolve disputes, troubleshoot problems, assist with any investigations, enforce the Mazed [Terms and Conditions](./terms-of-service), and take other actions otherwise permitted by law.
+Upon User’s written request, Mazed will close User’s account and remove User’s Provided Data from view as soon as reasonably practicable, based on User’s account activity and in accordance with applicable law. However, User acknowledges, understands and consents that Mazed may retain User’s Provided Data from closed accounts to comply with the law, prevent fraud, collect any fees owed, resolve disputes, troubleshoot problems, assist with any investigations, enforce the Mazed Terms and Conditions, and take other actions otherwise permitted by law.
 
 We store the Provided Data we receive as described in this Privacy Policy for as long as you use our Services or as necessary to fulfill the purpose(s) for which it was collected, provide our Services, resolve disputes, establish legal defenses, conduct audits, pursue legitimate business purposes, enforce our agreements, and comply with applicable laws.
 
 
-## **Disclosure**
+## Disclosure
 
 Mazed may disclose Provided Data to respond to legal requirements, to enforce our policies, or to protect anyone’s rights or property. Accordingly, User’s information may be shared with:
 
@@ -83,27 +94,29 @@ Mazed may disclose Provided Data to respond to legal requirements, to enforce ou
 Without limiting the authorizations under section “Security” hereof, and in an effort to respect User’s privacy and our ability to keep the community free from bad actors, Mazed will not otherwise disclose User’s Provided Data to law enforcement, other government officials, or other third parties without a subpoena, court order, or substantially similar legal procedure, except when we believe in good faith that the disclosure of information is necessary to prevent imminent physical harm or financial loss or to report suspected illegal activity.
 
 
-## **Security**
+## Security
 
 All information processed by us may be transferred, processed, and stored anywhere in the world, including but not limited to, the United States or other countries, which may have data protection laws that are different from the laws where you live.
 
 We treat data as an asset that must be protected and use encryption, passwords, physical security and other security protocols to protect Users’ Provided Data against unauthorized access and disclosure. However, third parties may unlawfully intercept or access transmission or private communications made between User and Mazed or between Users of Mazed, and other Users may abuse or misuse Users’ personal information that they collect from the sites. Therefore, to the extent permitted by law, User agrees to hold harmless Mazed in the event that a third party unlawfully intercepts or accesses any information provided by User to Mazed.
 
-**Account Access. **
+
+### Account Access
 
 The User’s password is the key to the User's account. The User should use unique numbers, letters and special characters, and should not disclose its Mazed account password to anyone. If User does share its password or its personal information with others, User acknowledges and understands that User is responsible for all action taken in the name of User’s account. If User loses control of its password, User may lose substantial control over its personal information and may be subject to legally binding actions taken on User’s behalf. If User’s password has been compromised for any reason, User should immediately contact and notify Mazed and User should change its password.
 
 
-## **Children’s Information**
+## Children’s Information
 
 The Services are not directed to children under 13 (or other age as required by local law), and we do not knowingly collect personal information from children. If you learn that your child has provided us with personal information without your consent, you may contact us as set forth below. If we learn that we have collected a child’s personal information without parental consent in violation of applicable law, we will promptly take steps to delete such information and terminate the child’s account.
 
 
-## **Legal Bases **
+## Legal Bases
 
 We only process your personal information when we believe it is necessary and we have a valid legal reason (i.e., legal basis) to do so under applicable law, like with your consent, to comply with laws, to provide you with services to enter into or fulfill our contractual obligations, to protect your rights, or to fulfill our legitimate business interests.
 
-If you are located in the EU or UK, this section applies to you.
+
+### If you are located in the EU or UK, this section applies to you.
 
 The General Data Protection Regulation (GDPR) and UK GDPR require us to explain the valid legal bases we rely on in order to process your personal information. As such, we may rely on the following legal bases to process your personal information:
 
@@ -128,11 +141,13 @@ Vital Interests. We may process your information where we believe it is necessar
 
 In legal terms, we are generally the "data controller" under European data protection laws of the personal information described in this privacy notice, since we determine the means and/or purposes of the data processing we perform. This privacy notice does not apply to the personal information we process as a "data processor" on behalf of our customers. In those situations, the customer that we provide services to and with whom we have entered into a data processing agreement is the "data controller" responsible for your personal information, and we merely process your information on their behalf in accordance with your instructions. If you want to know more about our customers' privacy practices, you should read their privacy policies and direct any questions you have to them.
 
-**If you are located in Canada, this section applies to you.**
+
+### If you are located in Canada, this section applies to you.
 
 We may process your information if you have given us specific permission (i.e., express consent) to use your personal information for a specific purpose, or in situations where your permission can be inferred (i.e., implied consent). You can withdraw your consent at any time.
 
 In some exceptional cases, we may be legally permitted under applicable law to process your information without your consent, including, for example:
+
 
 * If collection is clearly in the interests of an individual and consent cannot be obtained in a timely way.
 * For investigations and fraud detection and prevention.
@@ -146,7 +161,8 @@ In some exceptional cases, we may be legally permitted under applicable law to p
 * If the collection is solely for journalistic, artistic, or literary purposes.
 * If the information is publicly available and is specified by the regulations.
 
-**If you are located in California, this section applies to you.**
+
+### If you are located in California, this section applies to you.
 
 We only process your personal information when we believe it is necessary and we have a valid legal reason (ie. legal basis) to do so under applicable law, like with your consent, to comply with laws to provide you with services to enter into or fulfill our contractual obligations, to protect your rights, or to fulfill our legitimate business interests.
 
@@ -157,14 +173,14 @@ addresses of all third parties with which we shared personal information in the 
 If you are under 18 years of age, reside in California, and have a registered account with Services, you have the right to request the removal of unwanted data that you publicly post on the Services. To request removal of such data, please contact us using the contact information provided below and include the email address associated with your account and a statement that you reside in California. We will make sure the data is not publicly displayed on the Services, but please be aware that the data may not be completely or comprehensively removed from all our systems (e.g., backups, etc.).
 
 
-### **CCPA Privacy Notice**
+### CCPA Privacy Notice
 
 The California Code of Regulations defines a "resident" as: (1) every individual who is in the State of California for other than a temporary or transitory purpose and (2) every individual who is domiciled in the State of California who is outside the State of California for a (1) every individual who is in the State of California for other than a temporary or transitory purpose and (2) every individual who is domiciled in the State of California who is outside the State of California for temporary or transitory purpose. All other individuals are defined as "non-residents.
 
 If this definition of "resident" applies to you, we must adhere to certain rights and obligations regarding your personal information.
 
 
-## **Copyright**
+## Copyright
 
 Some data available on Mazed is protected by copyright laws and may contain material proprietary to ASG Mazed, Inc. This data or any of its components may not be reproduced, republished, distributed, transmitted, displayed, broadcasted, or otherwise disseminated or exploited in any manner without the express prior written permission of ASG Mazed Inc. The receipt or possession of this data or any of its components does not convey any rights to reproduce, disclose, or distribute its contents, or to manufacture, use, or sell anything that it may describe, in whole or in part. If consent to use specific data is granted, a link to the current version of that data on the Mazed website must be included for reference and sourcing. If you have questions about the use or reproduction of data available on Mazed’s sites, please contact us.
 `
