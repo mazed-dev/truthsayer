mazed-dev/truthsayer

commit 7f0e4eeef5fca44158b9fcd69e3eb61f41d1fc89

don't logout on KnockerElement's useEffect cleanup (#490)

smuggler-api/src/auth/knocker.ts
@@ -82,12 +82,25 @@ export class Knocker {
     )
   }
 
-  abort = async () => {
+  /**
+   * Reverses the effect of @see start() with no side effects.
+   * @see abort() for a version with side effects.
+   */
+  stop = () => {
     if (this.#scheduledId) {
       clearTimeout(this.#scheduledId)
       this.#scheduledId = null
     }
     this.#abortController.abort()
+  }
+
+  /**
+   * Reverses the effect of @see start() & invokes the abort callback supplied
+   * during construction.
+   * @see stop() for a side effect-free version.
+   */
+  abort = async () => {
+    stop()
     if (this.#abortCallback) {
       this.#abortCallback()
     }

truthsayer/src/auth/Knocker.tsx
@@ -9,7 +9,7 @@ export function KnockerElement() {
     })
     knocker.start({})
     return () => {
-      knocker.abort()
+      knocker.stop()
     }
   }, [])
   return <></>

commit 53219b926cd90eda67514878f018cb54112d9bb5

add missing 'this' to Knocker.abort()
To fix a regression introduced in #490

smuggler-api/src/auth/knocker.ts
@@ -100,7 +100,7 @@ export class Knocker {
    * @see stop() for a side effect-free version.
    */
   abort = async () => {
-    stop()
+    this.stop()
     if (this.#abortCallback) {
       this.#abortCallback()
     }