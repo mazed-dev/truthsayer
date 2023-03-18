38b43dc177126d893ba502f4d59833dfc7b08354
Sergey Nikitin <snikitin@outlook.com>
2023-03-18 16:31:49 +0000

    prettier

--- archaeologist/src/background/auth.ts
+++ archaeologist/src/background/auth.ts
@@ -100,8 +100,7 @@ export async function check() {
   const user = await authentication.getAuth({}).catch(() => null)
   if (user != null) {
     await _loginHandler(user)
-  }
-  else {
+  } else {
     await _logoutHandler()
   }
 }
