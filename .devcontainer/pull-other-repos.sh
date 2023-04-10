67fed2babfd647cfa1ea3ea840bc01c2c29305a9
Sergey Nikitin <snikitin@outlook.com>
2023-04-10 09:15:55 +0000

    test devcontainer files

---
+++ .devcontainer/pull-other-repos.sh
@@ -0,0 +1,7 @@
+# Clone other Mazed repos alongside truthsayer so they are all at the same
+# folder level, e.g.
+# \--mydir
+#       \-- truthsayer
+#       \-- smuggler
+#       \-- something-else
+git clone https://github.com/mazed-dev/smuggler.git /workspaces/smuggler
