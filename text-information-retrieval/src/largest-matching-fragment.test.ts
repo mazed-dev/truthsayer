c55cabe884e131bef88fbd9d1340d134fd04afaa
akindyakov <akindyakov@gmail.com>
2023-04-10 13:56:20 +0100

    Move code to the lib from tests file

--- text-information-retrieval/src/largest-matching-fragment.test.ts
+++ text-information-retrieval/src/largest-matching-fragment.test.ts
@@ -1,292 +1,168 @@
-// Load wink-nlp package.
+import {
+  impl,
+  findLargestCommonContinuousSubsequenceOfStems,
+} from './largest-matching-fragment'
 import winkNLP from 'wink-nlp'
-// Load english language model.
 import model from 'wink-eng-lite-web-model'
-import { range } from 'armoury'
 
-function findLargestCommonSubsequenceIndexes<T>(
-  arr1: T[],
-  arr2: T[]
-): number[] {
-  const m = arr1.length
-  const n = arr2.length
-
-  // Initialize the table with zeros
-  const table: number[][] = []
-  for (let i = 0; i <= m; i++) {
-    table[i] = []
-    for (let j = 0; j <= n; j++) {
-      table[i][j] = 0
-    }
-  }
-
-  // Fill in the table using dynamic programming
-  for (let i = 1; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      if (arr1[i - 1] === arr2[j - 1]) {
-        table[i][j] = table[i - 1][j - 1] + 1
-      } else {
-        table[i][j] = Math.max(table[i - 1][j], table[i][j - 1])
-      }
-    }
-  }
-
-  // Trace back the table to find the subsequence indexes
-  const result: number[] = []
-  let i = m
-  let j = n
-  while (i > 0 && j > 0) {
-    if (arr1[i - 1] === arr2[j - 1]) {
-      result.unshift(i - 1)
-      i--
-      j--
-    } else if (table[i - 1][j] > table[i][j - 1]) {
-      i--
-    } else {
-      j--
-    }
-  }
-  return result
-}
-
-function findLargestCommonContinuousSubsequenceIndexes<T>(
-  arr1: T[],
-  arr2: T[]
-): number[] {
-  const m = arr1.length
-  const n = arr2.length
-
-  // Initialize the table with zeros
-  const table: number[][] = []
-  for (let i = 0; i <= m; i++) {
-    table[i] = []
-    for (let j = 0; j <= n; j++) {
-      table[i][j] = 0
-    }
-  }
-
-  let maxLen = 0 // Length of the largest common continuous subsequence
-  let maxI = 0 // Ending index of the largest common continuous subsequence in the first array
-
-  // Fill in the table using dynamic programming
-  for (let i = 1; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      if (arr1[i - 1] === arr2[j - 1]) {
-        table[i][j] = table[i - 1][j - 1] + 1
-        if (table[i][j] > maxLen) {
-          maxLen = table[i][j]
-          maxI = i - 1
-        }
-      } else {
-        table[i][j] = 0
-      }
-    }
-  }
-
-  // Construct the result array
-  const result: number[] = []
-  for (let i = maxI - maxLen + 1; i <= maxI; i++) {
-    result.push(i)
-  }
-
-  return result
-}
-
-function splitIntoContinuousIntervals(row: number[]): number[][] {
-  const intervals: number[][] = []
-  let current: number[] = []
-  for (const item of row) {
-    if (current.length === 0) {
-      current.push(item)
-    } else {
-      const prev = current[current.length - 1]
-      if (item === prev + 1) {
-        current.push(item)
-      } else {
-        intervals.push(current)
-        current = [item]
-      }
-    }
-  }
-  if (current.length !== 0) {
-    intervals.push(current)
-  }
-  return intervals
-}
-
-function fillSmallGaps(row: number[], maxGapLen: number): number[] {
-  const res: number[] = []
-  for (const item of row) {
-    if (res.length !== 0) {
-      const prev = res[res.length - 1]
-      if (item <= prev + maxGapLen) {
-        // Fill the small gap
-        for (const v of range(prev + 1, item)) {
-          res.push(v)
-        }
-      }
-    }
-    res.push(item)
-  }
-  return res
-}
-
-function extendInterval(
-  row: number[],
-  prefixLen: number,
-  suffixLen: number,
-  maxItem: number
-): number[] {
-  const first = row[0]
-  const last = row[row.length - 1]
-  for (const item of range(Math.max(0, first - prefixLen), first)) {
-    row.unshift(item)
-  }
-  for (const item of range(
-    Math.min(last + 1, maxItem),
-    Math.min(maxItem, last + suffixLen + 1)
-  )) {
-    row.push(item)
-  }
-  return row
-}
-
-function sortOutSpacesAroundPunctuation(str: string): string {
-  return str
-    .replace(/\s*"\s*(.*?)\s*"/g, ' "$1" ')
-    .replace(/\s*'\s*(.*?)\s*'/g, " '$1' ")
-    .replace(/\s*([«“„〝])\s*(.*?)\s*([»”‟〞])/g, ' $1$2$3 ')
-    .replace(/\s*([:;!?.,…])\s*/g, '$1 ')
-    .replace(/\s+(['ʼ])\s+/g, '$1')
-    .replace(/\s\s+/g, ' ')
-    .trim()
-}
-
-describe('', () => {
+describe('Find largest matching fragment of text', () => {
+  const wink = winkNLP(model)
+  it('extendInterval', () => {
+    expect(impl.extendInterval([1], 1, 1, 10)).toStrictEqual([0, 1, 2])
+    expect(impl.extendInterval([1, 2], 1, 1, 10)).toStrictEqual([0, 1, 2, 3])
+    expect(impl.extendInterval([1, 2], 1, 2, 10)).toStrictEqual([0, 1, 2, 3, 4])
+    expect(impl.extendInterval([1, 2], 2, 2, 10)).toStrictEqual([0, 1, 2, 3, 4])
+    expect(impl.extendInterval([1, 2], 2, 2, 3)).toStrictEqual([0, 1, 2, 3])
+    expect(impl.extendInterval([10, 11], 3, 5, 100)).toStrictEqual([
+      7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+    ])
+  })
   it('fillSmallGaps', () => {
-    expect(fillSmallGaps([], 1)).toStrictEqual([])
-    expect(fillSmallGaps([], 2)).toStrictEqual([])
-    expect(fillSmallGaps([1], 2)).toStrictEqual([1])
-    expect(fillSmallGaps([1, 2], 1)).toStrictEqual([1, 2])
-    expect(fillSmallGaps([1, 2, 4], 2)).toStrictEqual([1, 2, 3, 4])
-    expect(fillSmallGaps([1, 2, 5], 2)).toStrictEqual([1, 2, 5])
-    expect(fillSmallGaps([1, 2, 5], 3)).toStrictEqual([1, 2, 3, 4, 5])
-    expect(fillSmallGaps([1, 2, 5, 8], 3)).toStrictEqual([
+    expect(impl.fillSmallGaps([], 1)).toStrictEqual([])
+    expect(impl.fillSmallGaps([], 2)).toStrictEqual([])
+    expect(impl.fillSmallGaps([1], 2)).toStrictEqual([1])
+    expect(impl.fillSmallGaps([1, 2], 1)).toStrictEqual([1, 2])
+    expect(impl.fillSmallGaps([1, 2, 4], 2)).toStrictEqual([1, 2, 3, 4])
+    expect(impl.fillSmallGaps([1, 2, 5], 2)).toStrictEqual([1, 2, 5])
+    expect(impl.fillSmallGaps([1, 2, 5], 3)).toStrictEqual([1, 2, 3, 4, 5])
+    expect(impl.fillSmallGaps([1, 2, 5, 8], 3)).toStrictEqual([
       1, 2, 3, 4, 5, 6, 7, 8,
     ])
-    expect(fillSmallGaps([1, 3, 5, 7], 2)).toStrictEqual([1, 2, 3, 4, 5, 6, 7])
-    expect(fillSmallGaps([0, 2, 4, 6, 8], 2)).toStrictEqual([
+    expect(impl.fillSmallGaps([1, 3, 5, 7], 2)).toStrictEqual([
+      1, 2, 3, 4, 5, 6, 7,
+    ])
+    expect(impl.fillSmallGaps([0, 2, 4, 6, 8], 2)).toStrictEqual([
       0, 1, 2, 3, 4, 5, 6, 7, 8,
     ])
   })
   it('splitIntoContinuousIntervals', () => {
-    expect(splitIntoContinuousIntervals([])).toStrictEqual([])
-    expect(splitIntoContinuousIntervals([1])).toStrictEqual([[1]])
-    expect(splitIntoContinuousIntervals([1, 2])).toStrictEqual([[1, 2]])
-    expect(splitIntoContinuousIntervals([1, 2, 4])).toStrictEqual([[1, 2], [4]])
-    expect(splitIntoContinuousIntervals([1, 2, 4, 5])).toStrictEqual([
+    expect(impl.splitIntoContinuousIntervals([])).toStrictEqual([])
+    expect(impl.splitIntoContinuousIntervals([1])).toStrictEqual([[1]])
+    expect(impl.splitIntoContinuousIntervals([1, 2])).toStrictEqual([[1, 2]])
+    expect(impl.splitIntoContinuousIntervals([1, 2, 4])).toStrictEqual([
+      [1, 2],
+      [4],
+    ])
+    expect(impl.splitIntoContinuousIntervals([1, 2, 4, 5])).toStrictEqual([
       [1, 2],
       [4, 5],
     ])
-    expect(splitIntoContinuousIntervals([1, 2, 4, 5, 7])).toStrictEqual([
+    expect(impl.splitIntoContinuousIntervals([1, 2, 4, 5, 7])).toStrictEqual([
       [1, 2],
       [4, 5],
       [7],
     ])
-    expect(splitIntoContinuousIntervals([1, 2, 4, 7, 8])).toStrictEqual([
+    expect(impl.splitIntoContinuousIntervals([1, 2, 4, 7, 8])).toStrictEqual([
       [1, 2],
       [4],
       [7, 8],
     ])
   })
   it('sortOutSpacesAroundPunctuation', () => {
-    expect(sortOutSpacesAroundPunctuation('')).toStrictEqual('')
-    expect(sortOutSpacesAroundPunctuation('Abc bcd.')).toStrictEqual('Abc bcd.')
+    expect(impl.sortOutSpacesAroundPunctuation('')).toStrictEqual('')
+    expect(impl.sortOutSpacesAroundPunctuation('Abc bcd.')).toStrictEqual(
+      'Abc bcd.'
+    )
     expect(
-      sortOutSpacesAroundPunctuation(
+      impl.sortOutSpacesAroundPunctuation(
         'Mileena returned in Mortal Kombat 11 … First . second ! Is it the last one ? '
       )
     ).toStrictEqual(
       'Mileena returned in Mortal Kombat 11… First. second! Is it the last one?'
     )
-    expect(sortOutSpacesAroundPunctuation(` " a " ' abc abc ' `)).toStrictEqual(
-      `"a" 'abc abc'`
-    )
     expect(
-      sortOutSpacesAroundPunctuation(
+      impl.sortOutSpacesAroundPunctuation(` " a " ' abc abc ' `)
+    ).toStrictEqual(`"a" 'abc abc'`)
+    expect(
+      impl.sortOutSpacesAroundPunctuation(
         `These are the colours I ' m talking about : blue , red, yellow ! Yan said " they all need just a few tweaks " . `
       )
     ).toStrictEqual(
       `These are the colours I'm talking about: blue, red, yellow! Yan said "they all need just a few tweaks".`
     )
   })
-  it('', () => {
-    const wink = winkNLP(model)
-    const first = `Jinx was added as a playable champion to the marksman roster of League of Legends in October 2013. As established in the lore written by Graham McNeill, Jinx was once a young innocent girl from Zaun, the seedy underbelly of the utopian city of Piltover. She harbors a dark and mysterious past with Vi, another champion from the game. Following a childhood tragedy, Jinx grew up to become "manic and impulsive" and her capacity for creating mayhem "became the stuff of legend".`
-    const second = `Jinx is a character in Riot Games' video game League of Legends and its associated media franchise. She first appeared in the music video "Get Jinxed" to commemorate her official debut as a playable champion in the game's October 2013 update. Jinx is depicted as a manic and impulsive criminal from Zaun who serves as the archenemy of the Piltover enforcer Vi.`
-    const firstDoc = wink.readDoc(first)
-    const secondDoc = wink.readDoc(second)
+  it('findLargestCommonContinuousSubsequenceOfStems - Jinx', () => {
+    const first = `Jinx was added as a playable champion to the marksman roster of League of Legends in October 2013. As established in the lore written by Graham McNeill, Jinx was once a young innocent girl from Zaun, the seedy underbelly of the utopian city of Piltover. She harbors a dark and mysterious past with Vi, another champion from the game. Following a childhood tragedy, Jinx grew up to become "manic and impulsive" and her capacity for creating mayhem "became the stuff of legend".
 
-    const firstTokens = firstDoc.tokens().out()
-    const firstPos = firstDoc.tokens().out(wink.its.pos)
-    const firstTypes = firstDoc.tokens().out(wink.its.type)
-    const firstStopWords = firstDoc.tokens().out(wink.its.stopWordFlag)
+The first season of Arcane reveals that Jinx was originally named Powder.[15] She and her older sister Violet "Vi" were orphaned following the repressed undercity's failed uprising against the utopian city of Piltover, after which they were taken in by Vander, the leader of the rebellion.
+        `
+    const second = `Jinx is a character in Riot Games' video game League of Legends and its associated media franchise. She first appeared in the music video "Get Jinxed" to commemorate her official debut as a playable champion in the game's October 2013 update. Jinx is depicted as a manic and impulsive criminal from Zaun who serves as the archenemy of the Piltover enforcer Vi.
 
-    let indexes = findLargestCommonSubsequenceIndexes(
-      firstDoc.tokens().out(wink.its.stem),
-      secondDoc.tokens().out(wink.its.stem)
+
+Jinx was one of the first champions from League of Legends to star in her own animated music video in the lead-up to her in-game debut. "Get Jinxed" by Agnete Kjølsrud from the band Djerv, which follows Jinx's destructive exploits in Piltover, was released on YouTube on October 8, 2013.
+    `
+
+    expect(
+      findLargestCommonContinuousSubsequenceOfStems(
+        first,
+        second,
+        wink,
+        10,
+        2,
+        6
+      ).extended
+    ).toStrictEqual(
+      `…, another champion from the game. Following a childhood tragedy, Jinx grew up to become "manic and impulsive" and her capacity for creating…`
     )
+  })
+  it('findLargestCommonContinuousSubsequenceOfStems - The Verge - Tesla', () => {
+    const first = `
+Tesla rewrote its own software to survive the chip shortage
+/ The company was able to swap substitute chips after rewriting its firmware
+By ANDREW J. HAWKINS / @andyjayhawk
 
-    indexes = indexes.filter((item) => {
-      return (
-        firstPos[item] !== 'PUNCT' &&
-        firstPos[item] !== 'PART' &&
-        !firstStopWords[item]
-      )
-    })
-    // const kExpectedLen = 42
-    const prefix = ''
-    const suffix = ''
-    // if (indexes.length < kExpectedLen) {
-    //   const half = kExpectedLen / 2
-    //   const startInd = Math.max(indexes[0] - half, 0)
-    //   const tailLen = Math.max(indexes[0] - startInd, half)
-    //   const endInd = Math.min(
-    //     indexes[indexes.length - 1] + tailLen,
-    //     firstTokens.length
-    //   )
-    //   indexes = range(startInd, endInd)
-    //   if (startInd > 0) {
-    //     prefix = '…'
-    //   }
-    //   if (endInd < firstTokens.length) {
-    //     suffix = '…'
-    //   }
-    // }
+Photo by Sean O’Kane / The Verge
+Tesla is weathering the global chip shortage by rewriting its vehicle software to support alternative chips, CEO Elon Musk said during an earnings call Monday. The shortage has upended the auto industry at a time of historic demand for new cars, leading to factory shutdowns, longer wait times, and higher prices.
 
-    console.log('Stem 1', firstDoc.tokens().out(wink.its.stem))
-    console.log('Stem 2', secondDoc.tokens().out(wink.its.stem))
-    console.log('Out 1', firstDoc.tokens().out())
-    console.log('Out 2', secondDoc.tokens().out())
-    console.log('Pos 1', firstPos)
-    console.log('Pos 1', firstTypes)
-    console.log('Result', indexes) // .map(item => firstDoc.tokens().itemAt(item)))
-    console.log(
-      'Result:',
-      `${prefix}…${sortOutSpacesAroundPunctuation(
-        indexes.map((item) => firstTokens[item]).join(' ')
-      )}…${suffix}`
+“We were able to substitute alternative chips, and then write the firmware in a matter of weeks,” Musk said. “It’s not just a matter of swapping out a chip; you also have to rewrite the software.”
+
+This approach has helped Tesla maintain high levels of production, delivering over 200,000 vehicles to customers over the course of the last three months, the company said. Tesla generated $11.9 billion in revenue in the quarter, including $1.1 billion in profit.
+
+Tesla isn’t alone in feeling the effects of the global shortage. With demand for cars at an all-time high, automakers around the world are feeling the constraints of production with chips in short supply. This week, Daimler and BMW said the lack of chips has forced it to shutdown some of their assembly lines, which will cut the companies’ output by tens of thousands of vehicles.
+
+“The global chip shortage situation remains quite serious”
+
+Musk said that Tesla’s future growth will depend on a swift resolution to the global semiconductor shortage. “The global chip shortage situation remains quite serious,” he said. “For the rest of this year, our growth rate will be determined by the slowest part in our supply chain,” which includes the wide range of chips used in Tesla’s vehicles.
+
+Tesla relies on chips to power everything from its airbags to the modules that control the vehicles’ seatbelts — which now means Tesla is missing components that are essential for the vehicle’s safety features. “A big struggle this quarter was the module that controls the airbags and seatbelts,” Musk said. “And obviously you cannot ship a car without those.”
+
+Musk sounded an uncertain note about the future. “It does seem like it’s getting better,” he said, “but it’s hard to predict.”
+  `
+
+    const second = `
+  Tesla employees reportedly passed around personal videos from owners’ cars
+/ Tesla workers viewed and shared private videos of car crashes, road rage incidents, and other potentially embarrassing clips, according to a report from Reuters.
+By EMMA ROTH
+
+This is a stock image of the Tesla logo spelled out in red with a white shape forming around it and a tilted and zoomed red Tesla T logo behind it.
+Illustration by Alex Castro / The Verge
+Tesla employees passed around and poked fun at private videos recorded by vehicle cameras, according to a report from Reuters. The videos, which were reportedly shared via Tesla’s internal messaging systems from 2019 through 2022, were recorded on the cameras that come mounted on Tesla vehicles to enable self-driving features.
+
+As described by sources to Reuters, the recordings shared by Tesla workers range from graphic crashes and road rage incidents to more embarrassing scenes, including a video of a naked man approaching a car. Some employees reportedly even created memes using captures from recorded videos and later shared them in private group chats.
+
+Tesla says “Sentry Mode recordings are not transmitted to us”
+
+One former Tesla employee tells Reuters that some of the videos may have even been recorded when vehicles were turned off. “We could see inside people’s garages and their private properties,” a former employee tells Reuters. “Let’s say that a Tesla customer had something in their garage that was distinctive, you know, people would post those kinds of things.”
+
+According to Reuters, Tesla previously had a policy that allowed the company to receive recordings from non-running vehicles if customers signed off on it. After an investigation from the Dutch Data Protection Authority (DPA) found that Tesla vehicles “were often filming everyone who came near the vehicle,” Tesla was turning off its vehicles’ cameras by default by 2023.
+
+Tesla launched Sentry Mode in 2019, advertising the feature as a way to alert drivers of any suspicious activity around their parked vehicles and then store recorded incidents in the car’s onboard memory. Tesla updated this feature in 2021 and started letting drivers use their vehicles’ cameras to livestream their cars’ surroundings from the Tesla app.
+
+On its support page for the feature, Tesla says “Sentry Mode recordings are not transmitted to us” while adding that livestreams are end-to-end encrypted and “cannot be accessed” by the company. Tesla also added a couple of other privacy-focused tweaks to Sentry Mode following the DPA’s investigation. Now cameras only start recording when the vehicle is touched, instead of right when it detects suspicious activity. Tesla also started warning passersby that its vehicles are recording by making their headlights flash.
+
+The Netherlands isn’t the only country where Tesla’s Sentry Mode has raised concerns. Last year, Germany’s consumer organization VZBZ sued Tesla, claiming Sentry Mode “violates data protection law.” Tesla vehicles were also banned from China’s Beidaihe district last year over concerns that the vehicles’ cameras would capture a private meeting between the country’s senior leadership, while the Chinese military banned Tesla vehicles in 2021 over similar surveillance concerns.
+  `
+    expect(
+      findLargestCommonContinuousSubsequenceOfStems(
+        first,
+        second,
+        wink,
+        10,
+        2,
+        8
+      ).extended
+    ).toStrictEqual(
+      `…/ The Verge. Tesla is weathering the global chip shortage by rewriting its vehicle software to support alternative chips, CEO Elon…`
     )
-    for (const interval of splitIntoContinuousIntervals(
-      fillSmallGaps(indexes, 8)
-    )) {
-      const extended = extendInterval(interval, 2, 2, firstTokens.length - 1)
-      console.log(
-        'Result: ',
-        `…${sortOutSpacesAroundPunctuation(
-          extended.map((item) => firstTokens[item]).join(' ')
-        )}…`
-      )
-    }
   })
 })
---
+++ text-information-retrieval/src/largest-matching-fragment.ts
@@ -0,0 +1,214 @@
+import { /* winkNLP, */ WinkMethods } from 'wink-nlp'
+// import model from 'wink-eng-lite-web-model'
+import { range /* log */ } from 'armoury'
+
+export namespace impl {
+  export function findLargestCommonSubsequenceIndexes<T>(
+    arr1: T[],
+    arr2: T[]
+  ): number[] {
+    const m = arr1.length
+    const n = arr2.length
+
+    // Initialize the table with zeros
+    const table: number[][] = []
+    for (let i = 0; i <= m; i++) {
+      table[i] = []
+      for (let j = 0; j <= n; j++) {
+        table[i][j] = 0
+      }
+    }
+
+    // Fill in the table using dynamic programming
+    for (let i = 1; i <= m; i++) {
+      for (let j = 1; j <= n; j++) {
+        if (arr1[i - 1] === arr2[j - 1]) {
+          table[i][j] = table[i - 1][j - 1] + 1
+        } else {
+          table[i][j] = Math.max(table[i - 1][j], table[i][j - 1])
+        }
+      }
+    }
+
+    // Trace back the table to find the subsequence indexes
+    const result: number[] = []
+    let i = m
+    let j = n
+    while (i > 0 && j > 0) {
+      if (arr1[i - 1] === arr2[j - 1]) {
+        result.unshift(i - 1)
+        i--
+        j--
+      } else if (table[i - 1][j] > table[i][j - 1]) {
+        i--
+      } else {
+        j--
+      }
+    }
+    return result
+  }
+
+  export function splitIntoContinuousIntervals(row: number[]): number[][] {
+    const intervals: number[][] = []
+    let current: number[] = []
+    for (const item of row) {
+      if (current.length === 0) {
+        current.push(item)
+      } else {
+        const prev = current[current.length - 1]
+        if (item === prev + 1) {
+          current.push(item)
+        } else {
+          intervals.push(current)
+          current = [item]
+        }
+      }
+    }
+    if (current.length !== 0) {
+      intervals.push(current)
+    }
+    return intervals
+  }
+
+  export function fillSmallGaps(row: number[], maxGapLen: number): number[] {
+    const res: number[] = []
+    for (const item of row) {
+      if (res.length !== 0) {
+        const prev = res[res.length - 1]
+        if (item <= prev + maxGapLen) {
+          // Fill the small gap
+          for (const v of range(prev + 1, item)) {
+            res.push(v)
+          }
+        }
+      }
+      res.push(item)
+    }
+    return res
+  }
+
+  export function extendInterval(
+    row: number[],
+    prefixLen: number,
+    suffixLen: number,
+    maxItem: number
+  ): number[] {
+    if (row.length === 0) {
+      return row
+    }
+    const first = row[0]
+    const last = row[row.length - 1]
+    const prefix: number[] = []
+    for (const item of range(Math.max(0, first - prefixLen), first)) {
+      prefix.push(item)
+    }
+    row.unshift(...prefix)
+    for (const item of range(
+      Math.min(last, maxItem),
+      Math.min(maxItem, last + suffixLen)
+    )) {
+      row.push(item + 1)
+    }
+    return row
+  }
+
+  export function sortOutSpacesAroundPunctuation(str: string): string {
+    return str
+      .replace(/\s*"\s*(.*?)\s*"/g, ' "$1" ') // ' abc ' -> 'abc'
+      .replace(/\s*'\s*(.*?)\s*'/g, " '$1' ") // " abc " -> "abc"
+      .replace(/\s*([«“„〝])\s*(.*?)\s*([»”‟〞])/g, ' $1$2$3 ')
+      .replace(/\s+([:;!?.,…'ʼ’])\s+/g, '$1 ')
+      .replace(/\s*(['ʼ’])\s*(ll|s|m|d)/g, '$1$2')
+      .replace(/\s\s+/g, ' ')
+      .trim()
+  }
+} // namespace impl
+
+type LargestCommonContinuousSubsequenceOfStems = {
+  extended: string
+  realMatchLen: number // As string without spaces
+}
+
+/**
+ * Returns largest substring of the first string that matches the second string
+ */
+export function findLargestCommonContinuousSubsequenceOfStems(
+  first: string,
+  second: string,
+  wink: WinkMethods,
+  gapToFillWordsNumber: number,
+  prefixToExtendCharsNumber: number,
+  suffixToExtendCharsNumber: number
+): LargestCommonContinuousSubsequenceOfStems {
+  // Clean up the inputs a bit
+  first = first.replace(/\n/g, '. ').replace(/\s+/g, ' ')
+  second = second.replace(/\n/g, '. ').replace(/\s+/g, ' ')
+
+  const firstDoc = wink.readDoc(first)
+  const secondDoc = wink.readDoc(second)
+
+  const firstTokens = firstDoc.tokens().out()
+  const firstPartsOfSpeach = firstDoc.tokens().out(wink.its.pos)
+  const firstStopWords = firstDoc.tokens().out(wink.its.stopWordFlag)
+
+  const indexes = impl
+    .findLargestCommonSubsequenceIndexes(
+      firstDoc.tokens().out(wink.its.stem),
+      secondDoc.tokens().out(wink.its.stem)
+    )
+    .filter((item) => {
+      // Remove matches for punctuation, spaces, symbols and stop words
+      return (
+        firstPartsOfSpeach[item] !== 'PUNCT' &&
+        firstPartsOfSpeach[item] !== 'SPACE' &&
+        firstPartsOfSpeach[item] !== 'SYM' &&
+        !firstStopWords[item]
+      )
+    })
+  // log.debug('Out 1', firstDoc.tokens().out())
+  // log.debug('Stem 1', firstDoc.tokens().out(wink.its.stem))
+  // log.debug('Stem 2', secondDoc.tokens().out(wink.its.stem))
+  // log.debug('Out 2', secondDoc.tokens().out())
+  // log.debug('Pos 1', firstPartsOfSpeach)
+  // log.debug('Types 1', firstTypes)
+  // log.debug('Result indexes', indexes)
+  const rows = impl.splitIntoContinuousIntervals(
+    impl.fillSmallGaps(indexes, gapToFillWordsNumber)
+  )
+  const lengths = rows.map((a) => a.length)
+  const largestInd = lengths.indexOf(Math.max(...lengths))
+  const largestRow = rows[largestInd]
+  const realMatchLen = largestRow
+    .map((index) => firstTokens[index].length)
+    .reduce((a: number, b: number) => a + b, 0)
+  // log.debug('Result longest row', largestInd)
+  // for (const ind in rows) {
+  //   const interval = rows[ind]
+  //   const extended = extendInterval(
+  //     interval,
+  //     prefixToExtendCharsNumber,
+  //     suffixToExtendCharsNumber,
+  //     firstTokens.length - 1
+  //   )
+  //   log.debug(
+  //     'Result: ',
+  //     ind,
+  //     `…${sortOutSpacesAroundPunctuation(
+  //       extended.map((item) => firstTokens[item]).join(' ')
+  //     )}…`
+  //   )
+  // }
+  const extended = impl.extendInterval(
+    largestRow,
+    prefixToExtendCharsNumber,
+    suffixToExtendCharsNumber,
+    firstTokens.length - 1
+  )
+  // Join string, sort out spaces and add ellipsises at the begining and the end
+  return {
+    extended: `…${impl.sortOutSpacesAroundPunctuation(
+      extended.map((item) => firstTokens[item]).join(' ')
+    )}…`,
+    realMatchLen,
+  }
+}
