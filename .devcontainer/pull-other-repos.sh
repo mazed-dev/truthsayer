553be76344a4ad546794a1ece2a5fe23386e105d
Sergey Nikitin <snikitin@outlook.com>
2023-04-10 10:34:29 +0000

    allow to cp files from codespace

--- .devcontainer/pull-other-repos.sh
+++ .devcontainer/pull-other-repos.sh
@@ -4,4 +4,9 @@
 #       \-- truthsayer
 #       \-- smuggler
 #       \-- something-else
-git clone https://github.com/mazed-dev/smuggler.git /workspaces/smuggler
+SMUGGLER_URL=https://github.com/mazed-dev/smuggler.git
+SMUGGLER_FOLDER=/workspaces/smuggler
+
+if ! git clone "${SMUGGLER_URL}" "${SMUGGLER_FOLDER}" 2>/dev/null && [ -d "${SMUGGLER_FOLDER}" ] ; then
+    echo "Clone failed because the folder ${SMUGGLER_FOLDER} exists"
+fi
