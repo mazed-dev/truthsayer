af0a7f2c5950c4ad5738432017d923294ce1aba0
akindyakov <akindyakov@gmail.com>
2023-05-26 16:43:49 +0100

    Done

--- archaeologist/src/content/augmentation/SuggestionsFloater.tsx
+++ archaeologist/src/content/augmentation/SuggestionsFloater.tsx
@@ -270,8 +270,10 @@ export const SuggestionsFloater = ({
 }) => {
   const nodeRef = React.useRef(null)
   const [controlledPosition, setControlledPosition] =
-    React.useState<Position2D | null>(null) // getStartDragPosition(false))
-  const [isRevealed, setRevealed] = React.useState<boolean>(defaultRevelaed && nodes.length)
+    React.useState<Position2D | null>(null)
+  // Floater can be open by default **only** if there is something to suggest.
+  defaultRevelaed = defaultRevelaed && nodes.length > 0
+  const [isRevealed, setRevealed] = React.useState<boolean>(defaultRevelaed)
 
   const analytics = React.useContext(ContentContext).analytics
   const saveRevealed = React.useCallback(
