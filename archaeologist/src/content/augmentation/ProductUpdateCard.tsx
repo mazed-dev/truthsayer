2a43cd71d6216f456ba04b561387f15196a96f1f
akindyakov <akindyakov@gmail.com>
2023-05-28 12:44:49 +0100

    Updated based on comments in slack under the demo

--- archaeologist/src/content/augmentation/ProductUpdateCard.tsx
+++ archaeologist/src/content/augmentation/ProductUpdateCard.tsx
@@ -3,17 +3,19 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import { Close } from '@emotion-icons/material'
+import { ContentContext } from '../context'
 
 import { ImgButton } from 'elementary'
-import { log } from 'armoury'
 import { SuggestedCardBox } from './SuggestedCard'
 
 import { ContentAugmentationProductUpdate } from './../../message/types'
 
-const kSignatureOfProductUpdateToShow = 'foreward-rebranding-26-05-2023'
+const kSignatureOfProductUpdateToShow = 'foreward-rebranding-28-05-2023'
 
 const Box = styled(SuggestedCardBox)`
-  background-image: linear-gradient(#e9f2fb, #1b7ff4);
+  background-image: linear-gradient(#c0defc, #005be3);
+  transition: height 500ms;
+  padding: 0;
 `
 
 const Header = styled.div`
@@ -34,12 +36,23 @@ const HeaderBtn = styled(ImgButton)`
 const BodyBox = styled.div`
   padding: 0 8px 8px 8px;
 `
-const Title = styled.h2`
+const HeaderTitle = styled.h2`
   font-size: 1.2em;
-  margin: 0 0 1em 0;
+  font-weight: 400;
+  margin: 0 0 0 1em;
 `
 const Message = styled.div``
 const MessageLink = styled.a``
+
+const ProductUpdateMiniCardBox = styled(Header)`
+  justify-content: space-between;
+  align-content: center;
+  align-items: center;
+  padding: 5px;
+  &:hover {
+    cursor: pointer;
+  }
+`
 
 export const ProductUpdateCard = ({
   productUpdateConfig,
@@ -50,7 +63,7 @@ export const ProductUpdateCard = ({
     update: ContentAugmentationProductUpdate | undefined
   ) => Promise<void>
 }) => {
-  log.debug('Render ProductUpdateCard ', productUpdateConfig)
+  const ctx = React.useContext(ContentContext)
   React.useEffect(() => {
     if (
       productUpdateConfig?.signature != null &&
@@ -60,31 +73,44 @@ export const ProductUpdateCard = ({
       updateProductUpdateConfig(undefined)
     }
   }, [productUpdateConfig?.signature, updateProductUpdateConfig])
-  const [isHidden, hide] = React.useState<boolean>(
+  const [isClosed, setClosed] = React.useState<boolean>(
     productUpdateConfig?.signature === kSignatureOfProductUpdateToShow
   )
-  // if (productUpdateConfig?.signature === kSignatureOfProductUpdateToShow) {
-  if (isHidden) {
+  const [isMiminised, setMiminised] = React.useState<boolean>(true)
+  const closeProductUpdate = React.useCallback(() => {
+    updateProductUpdateConfig({
+      signature: kSignatureOfProductUpdateToShow,
+    }).then(() => {
+      setClosed(true)
+    })
+    // To measure how many people saw the update and closed it.
+    ctx.analytics?.capture('Click product update close', {
+      'Event type': 'change',
+      isRevealed: !isMiminised,
+      productUpdateSignature: kSignatureOfProductUpdateToShow,
+    })
+  }, [updateProductUpdateConfig, setClosed, ctx.analytics, isMiminised])
+  if (isClosed) {
     return null
   }
-  log.debug('Render ProductUpdateCard')
   return (
     <Box>
-      <Header>
-        <HeaderBtn
-          onClick={() => {
-            updateProductUpdateConfig({
-              signature: kSignatureOfProductUpdateToShow,
-            }).then(() => {
-              hide(true)
-            })
-          }}
-        >
+      <ProductUpdateMiniCardBox
+        onClick={() => {
+          setMiminised((value) => !value)
+          // To measure how many people opened product update
+          ctx.analytics?.capture('Click product update visibility toggle', {
+            'Event type': 'change',
+            productUpdateSignature: kSignatureOfProductUpdateToShow,
+          })
+        }}
+      >
+        <HeaderTitle>Meet Foreword 🎉 </HeaderTitle>
+        <HeaderBtn onClick={closeProductUpdate}>
           <Close size="14px" />
         </HeaderBtn>
-      </Header>
-      <BodyBox>
-        <Title>Meet Foreword!</Title>
+      </ProductUpdateMiniCardBox>
+      <BodyBox css={{ display: isMiminised ? 'none' : 'block' }}>
         <Message>
           Here will be an outstanding text about rebrading with{' '}
           <MessageLink href="https://thinkforeword.com">
