772a86322c2f20e0ded80eddd1fd662f923b379c
Sergey Nikitin <snikitin@outlook.com>
2023-06-18 18:23:02 +0000

    turn on webgpu

--- archaeologist/src/background.ts
+++ archaeologist/src/background.ts
@@ -12,6 +12,7 @@ import { truthsayer } from 'elementary'
 import * as badge from './badge/badge'
 
 import browser, { Tabs } from 'webextension-polyfill'
+import '@tensorflow/tfjs-backend-webgpu'
 import {
   BackgroundAction,
   BackgroundActionProgress,
@@ -613,6 +614,15 @@ class Background {
 
     this.deinitialisers.push(browserBookmarks.register(ctx.storage))
     this.deinitialisers.push(webNavigation.register(ctx.storage))
+    try {
+      await tf.setBackend('webgpu')
+    } catch (error) {
+      backgroundpa.warning(ctx.analytics, {
+        failedTo: 'set tf backend to webgpu',
+        cause: errorise(error).message,
+        location: 'background',
+      })
+    }
     this.deinitialisers.push(
       await similarity.register(ctx.storage, ctx.analytics)
     )
