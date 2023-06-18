836c45a2e8652e9869a40bcc3086af72dc45078d
Sergey Nikitin <snikitin@outlook.com>
2023-06-18 18:23:48 +0000

    unused import

--- archaeologist/src/background.ts
+++ archaeologist/src/background.ts
@@ -38,7 +38,6 @@ import {
   processMsgFromMsgProxyStorageApi,
   UserAccount,
 } from 'smuggler-api'
-import { tf } from 'text-information-retrieval'
 
 import { makeBrowserExtStorageApi } from './storage_api_browser_ext'
 import { isReadyToBeAutoSaved } from './background/pageAutoSaving'
